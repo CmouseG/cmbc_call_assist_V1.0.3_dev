dfabfb01c5c69627305d378b98be54c7104d7fdc
tangtq <tangtaiquan@guiji.ai>
2018-10-19 09:52:43 +0800

    工程初始化

---
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/Application.java
@@ -0,0 +1,20 @@
+package com.guiji.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by ty on 2018/10/18.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ guiyu-parent/guiyu-robot-parent/guiyu-robot-web/src/main/java/com/guiji/robot/Application.java
@@ -0,0 +1,19 @@
+package com.guiji.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by ty on 2018/10/18.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

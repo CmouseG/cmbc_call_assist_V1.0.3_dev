2252c753ca251e982291f58cb08423209d772b98
gaojianfeng <417109124@qq.com>
2018-10-23 14:19:21 +0800

    init dispatch serviece

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/CommonResponse.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/CommonResponse.java
@@ -11,4 +11,27 @@ public class CommonResponse {
     //响应内容
     private Object content;
 
+    public String getRespCode() {
+        return respCode;
+    }
+
+    public void setRespCode(String respCode) {
+        this.respCode = respCode;
+    }
+
+    public String getRespMsg() {
+        return respMsg;
+    }
+
+    public void setRespMsg(String respMsg) {
+        this.respMsg = respMsg;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
 }
--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/Schedule.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/Schedule.java
@@ -52,4 +52,148 @@ public class Schedule {
     private String gmtCreate;
 
     private String gmtUpdate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPlanUuid() {
+        return planUuid;
+    }
+
+    public void setPlanUuid(String planUuid) {
+        this.planUuid = planUuid;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAttach() {
+        return attach;
+    }
+
+    public void setAttach(String attach) {
+        this.attach = attach;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+    public String getStatusPlan() {
+        return statusPlan;
+    }
+
+    public void setStatusPlan(String statusPlan) {
+        this.statusPlan = statusPlan;
+    }
+
+    public String getStatusSync() {
+        return statusSync;
+    }
+
+    public void setStatusSync(String statusSync) {
+        this.statusSync = statusSync;
+    }
+
+    public String getRecall() {
+        return recall;
+    }
+
+    public void setRecall(String recall) {
+        this.recall = recall;
+    }
+
+    public String getRecallParams() {
+        return recallParams;
+    }
+
+    public void setRecallParams(String recallParams) {
+        this.recallParams = recallParams;
+    }
+
+    public String getRobot() {
+        return robot;
+    }
+
+    public void setRobot(String robot) {
+        this.robot = robot;
+    }
+
+    public String getCallAgent() {
+        return callAgent;
+    }
+
+    public void setCallAgent(String callAgent) {
+        this.callAgent = callAgent;
+    }
+
+    public String getClean() {
+        return clean;
+    }
+
+    public void setClean(String clean) {
+        this.clean = clean;
+    }
+
+    public String getCallDate() {
+        return callDate;
+    }
+
+    public void setCallDate(String callDate) {
+        this.callDate = callDate;
+    }
+
+    public String getCallHour() {
+        return callHour;
+    }
+
+    public void setCallHour(String callHour) {
+        this.callHour = callHour;
+    }
+
+    public String getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(String gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public String getGmtUpdate() {
+        return gmtUpdate;
+    }
+
+    public void setGmtUpdate(String gmtUpdate) {
+        this.gmtUpdate = gmtUpdate;
+    }
 }

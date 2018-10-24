85c5b33ab7094e16336ad0495f0cac6c7db3b611
tangtq <tangtaiquan@guiji.ai>
2018-10-24 11:32:50 +0800

    init

--- guiyu-parent/guiyu-cloud-parent/guiyu-cloud-zuul/src/main/java/com/guiji/cloud/zuul/ZuulApplication.java
+++ guiyu-parent/guiyu-cloud-parent/guiyu-cloud-zuul/src/main/java/com/guiji/cloud/zuul/ZuulApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Bean;
  */
 @EnableZuulProxy
 @SpringCloudApplication
+@EnableEurekaClient
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ZuulApplication {
     public static void main(String[] args) {

af9e45063c177c332635b5f49a6dd1cc217592dd
xujin <728805437@qq.com>
2018-10-26 17:39:39 +0800

    add by xujin

---
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/dao/DispatchPlanMapper.java
@@ -0,0 +1,30 @@
+package com.guiji.dispatch.dao;
+
+import com.guiji.dispatch.dao.entity.DispatchPlan;
+import com.guiji.dispatch.dao.entity.DispatchPlanExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DispatchPlanMapper {
+    int countByExample(DispatchPlanExample example);
+
+    int deleteByExample(DispatchPlanExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(DispatchPlan record);
+
+    int insertSelective(DispatchPlan record);
+
+    List<DispatchPlan> selectByExample(DispatchPlanExample example);
+
+    DispatchPlan selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") DispatchPlan record, @Param("example") DispatchPlanExample example);
+
+    int updateByExample(@Param("record") DispatchPlan record, @Param("example") DispatchPlanExample example);
+
+    int updateByPrimaryKeySelective(DispatchPlan record);
+
+    int updateByPrimaryKey(DispatchPlan record);
+}

95c09dc4435ffe2bf0dcc64faf594953b7c25345
tangtq <tangtaiquan@guiji.ai>
2018-10-31 14:22:21 +0800

    Result

--- guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/result/Result.java
+++ guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/result/Result.java
@@ -19,9 +19,9 @@ public class Result {
 		return new Result.ReturnData<T>();
 	}
 	
-	public static Result.ReturnData<?> error(String code){
+	public static Result.ReturnData error(String code){
 		String msg=properties.getProperty(code);
-		return new Result.ReturnData<Object>(code,msg);
+		return new Result.ReturnData(code,msg);
 	}
 	
 	@Autowired(required = true)

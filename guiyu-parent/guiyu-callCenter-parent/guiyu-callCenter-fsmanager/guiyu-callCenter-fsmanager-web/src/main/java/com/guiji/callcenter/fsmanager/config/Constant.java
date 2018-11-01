02cf4b6bab746de85986eac66aa351fb18338b8d
sunsy <sunsiyuan0510@126.com>
2018-11-01 10:00:25 +0800

    修改fsagent、fsmanager代码结构

---
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-fsmanager/guiyu-callCenter-fsmanager-web/src/main/java/com/guiji/callcenter/fsmanager/config/Constant.java
@@ -0,0 +1,13 @@
+package com.guiji.callcenter.fsmanager.config;
+
+public class Constant {
+
+    public static final String CONFIG_TYPE_DIALPLAN = "dialplan";
+    public static final String CONFIG_TYPE_GATEWAY = "gateway";
+
+    public static final String PROTOCOL = "http://";
+
+    public static final String SERVER_NAME_FSAGENT = "guiyu-callcenter-fsagent";
+
+    public static final String ERROR_PARAM = "0303001";
+}
---
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-fsmanager/guiyu-callCenter-fsmanager-web/src/main/java/com/guiji/callcenter/fsmanager/entity/ConfigTypeEnum.java
@@ -0,0 +1,8 @@
+package com.guiji.callcenter.fsmanager.entity;
+
+public enum ConfigTypeEnum {
+
+    dialplan,//拨号方案
+
+    gateway;//网关
+}

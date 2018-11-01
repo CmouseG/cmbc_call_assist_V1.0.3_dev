4a15fe0f45dec8c25976ffef11334eb9b4710ba6
tangtq <tangtaiquan@guiji.ai>
2018-11-01 19:10:58 +0800

    异常码定义

--- guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/exception/GuiyuExceptionEnum.java
+++ guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/exception/GuiyuExceptionEnum.java
@@ -3,11 +3,15 @@ package com.guiji.common.exception;
 /**
  * Created by ty on 2018/11/1.
  */
-public enum GuiyuExceptionEnum {
+public enum GuiyuExceptionEnum implements ExceptionEnum {
     /**
      * 公共异常
      */
     EXCP_COMMON_USERNAME_OR_PASSWORD_ERR("00000001","用户名密码错误");
+
+    public String getName() {
+        return this.name();
+    }
 
     //返回码
     private String errorCode;
--- guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/result/ErrorMessage.java
+++ guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/result/ErrorMessage.java
@@ -13,17 +13,25 @@ import org.springframework.core.io.Resource;
 
 @Configuration
 public class ErrorMessage {
-	
+
+//	@Bean(name="errorProperties")
+//	public Properties init() throws IOException{
+//		Properties props=new Properties();
+//		Resource defalutResource=new ClassPathResource("properties");
+//		File file=defalutResource.getFile();
+//		File[] files=file.listFiles((dir,name)->{ return name.endsWith("properties"); });
+//		for(File fileItem:files){
+//			props.load(new InputStreamReader(new FileInputStream(fileItem), "utf-8"));
+//		}
+//
+//		return props;
+//	}
+
 	@Bean(name="errorProperties")
 	public Properties init() throws IOException{
 		Properties props=new Properties();
-		Resource defalutResource=new ClassPathResource("properties");
-		File file=defalutResource.getFile();
-		File[] files=file.listFiles((dir,name)->{ return name.endsWith("properties"); });
-		for(File fileItem:files){
-			props.load(new InputStreamReader(new FileInputStream(fileItem), "utf-8"));
-		}
-		
+		Resource defalutResource=new ClassPathResource("properties/error.properties");
+		props.load(new InputStreamReader(defalutResource.getInputStream(),"utf-8"));
 		return props;
 	}
 }

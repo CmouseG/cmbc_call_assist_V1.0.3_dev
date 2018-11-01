00f0e2447738957296514390fd0e86d1ad98fe3d
tangtq <tangtaiquan@guiji.ai>
2018-11-01 13:53:34 +0800

    影像服务

--- guiyu-parent/guiyu-nas-parent/guiyu-nas-web/src/main/java/com/guiji/NasApplication.java
+++ guiyu-parent/guiyu-nas-parent/guiyu-nas-web/src/main/java/com/guiji/NasApplication.java
@@ -4,16 +4,11 @@ import com.github.tobato.fastdfs.FdfsClientConfig;
 import com.guiji.common.result.EnableAutoResultPack;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
 
 /**
  * Created by ty on 2018/10/18.
@@ -24,8 +19,8 @@ import java.util.List;
 @EnableAutoResultPack
 @Import(FdfsClientConfig.class)
 @MapperScan("com.guiji.nas.dao")
-public class Application extends WebMvcConfigurerAdapter {
+public class NasApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(NasApplication.class, args);
     }
 }

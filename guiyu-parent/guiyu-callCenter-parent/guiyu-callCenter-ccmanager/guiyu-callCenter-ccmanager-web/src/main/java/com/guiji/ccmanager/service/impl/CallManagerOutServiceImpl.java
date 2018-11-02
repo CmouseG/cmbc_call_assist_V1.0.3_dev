16b42bda2091c850ab29e0f8ac3914f757fbcc2d
liyang <821927329@qq.com>
2018-11-02 11:37:30 +0800

    callcenter-ccmanager 提交

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/CallManagerOutServiceImpl.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/CallManagerOutServiceImpl.java
@@ -7,9 +7,11 @@ import com.guiji.callcenter.dao.entity.CallOutPlanExample;
 import com.guiji.callcenter.dao.entity.LineInfo;
 import com.guiji.calloutserver.api.ICallPlanApi;
 import com.guiji.ccmanager.constant.Constant;
-import com.guiji.ccmanager.feign.TempApiFeign;
 import com.guiji.ccmanager.service.CallManagerOutService;
 import com.guiji.common.result.Result;
+import com.guiji.fsmanager.api.ITempApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,12 +25,14 @@ import java.util.List;
 @Service
 public class CallManagerOutServiceImpl implements CallManagerOutService {
 
+    private final Logger log = LoggerFactory.getLogger(CallManagerOutServiceImpl.class);
+
     @Autowired
     private LineInfoMapper lineInfoMapper;
     @Autowired
     private CallOutPlanMapper callOutPlanMapper;
     @Autowired
-    private TempApiFeign tempApiFeign;
+    private ITempApi tempApiFeign;
     @Autowired
     private ICallPlanApi callPlanApi;
 
@@ -39,6 +43,7 @@ public class CallManagerOutServiceImpl implements CallManagerOutService {
         //根据线路id到lineinfo表中查询线路是否存在，不存在则返回线路不存在错误，并报警
         LineInfo lineInfoDB = lineInfoMapper.selectByPrimaryKey(Integer.parseInt(lineId));
         if(lineInfoDB==null){
+            log.warn("line not exist,lineId:"+lineId);
             return Result.error(Constant.ERROR_LINE_NOTEXIST);
         }
         //到calloutplan中查询该线路是否存在待呼叫或进行中的计划，存在则返回线路繁忙错误，并报警
@@ -48,6 +53,7 @@ public class CallManagerOutServiceImpl implements CallManagerOutService {
         criteria.andCallStateBetween(Constant.CALLSTATE_INIT,Constant.CALLSTATE_AGENT_ANSWER);
         List<CallOutPlan> existList = callOutPlanMapper.selectByExample(example);
         if(existList!=null && existList.size()>0){
+            log.warn("line is running,lineId:"+lineId);
             return Result.error(Constant.ERROR_LINE_RUNNING);
         }
 
@@ -55,11 +61,13 @@ public class CallManagerOutServiceImpl implements CallManagerOutService {
         Result.ReturnData<Boolean> resultTemp = tempApiFeign.istempexist(tempId);
         if(resultTemp.getCode().equals(Constant.SUCCESS_COMMON) && resultTemp.getBody() == false){
             //返回模板不存在错误，并报警
+            log.warn("temp is not exist,tempId:"+tempId);
             return Result.error(Constant.ERROR_TEMP_NOTEXIST);
         }
         //调用fsmanager的下载模板接口
         Result.ReturnData result = tempApiFeign.downloadtempwav(tempId);
         if(!result.getCode().equals(Constant.SUCCESS_COMMON)){
+            log.warn("tempApiFeign.downloadtempwav failed,code:"+result.getCode());
             return result;
         }
         //调用所有calloutserver的启动客户呼叫计划接口

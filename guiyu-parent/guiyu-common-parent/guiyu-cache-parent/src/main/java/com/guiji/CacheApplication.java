a0e4eaaeb8b2a4a4d109349ba66b6a35c2ec40d3
tangtq <tangtaiquan@guiji.ai>
2018-11-04 10:07:16 +0800

    缓存服务

---
+++ guiyu-parent/guiyu-common-parent/guiyu-cache-parent/src/main/java/com/guiji/CacheApplication.java
@@ -0,0 +1,20 @@
+package com.guiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by ty on 2018/10/18.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class CacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class, args);
+    }
+
+}

88a84f784618bcea8a9684a84ac9e9810ef635bf
liyang <821927329@qq.com>
2018-11-06 13:44:36 +0800

    callcenter-calloutservice提交

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-service/src/main/java/com/guiji/calloutserver/CalloutServerApplication.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-service/src/main/java/com/guiji/calloutserver/CalloutServerApplication.java
@@ -1,6 +1,6 @@
 package com.guiji.calloutserver;
 
-import com.guiji.common.result.EnableAutoResultPack;
+import com.guiji.component.result.EnableAutoResultPack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;

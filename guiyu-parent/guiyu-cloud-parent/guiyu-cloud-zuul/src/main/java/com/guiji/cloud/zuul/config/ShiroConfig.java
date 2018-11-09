46b63f42f660c63ba6f22b8f8e7960cf6f3dede1
mayufeng <278436995@qq.com>
2018-11-09 16:26:03 +0800

    放开权限

--- guiyu-parent/guiyu-cloud-parent/guiyu-cloud-zuul/src/main/java/com/guiji/cloud/zuul/config/ShiroConfig.java
+++ guiyu-parent/guiyu-cloud-parent/guiyu-cloud-zuul/src/main/java/com/guiji/cloud/zuul/config/ShiroConfig.java
@@ -49,7 +49,8 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/loginOut", "anon");
         filterChainDefinitionMap.put("/getUserId", "zuulAuthc");
         //主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截
-        filterChainDefinitionMap.put("/**", "zuulAuthc,zuulPerms");
+//        filterChainDefinitionMap.put("/**", "zuulAuthc,zuulPerms");
+        filterChainDefinitionMap.put("/**", "anon");
 
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return shiroFilterFactoryBean;

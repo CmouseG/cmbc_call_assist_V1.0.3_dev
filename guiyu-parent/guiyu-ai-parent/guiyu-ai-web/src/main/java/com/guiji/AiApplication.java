2d90d4dd61b36ad9c0e3da43196a7a0827a7b8ab
tangtq <tangtaiquan@guiji.ai>
2018-11-14 14:42:09 +0800

    AI

---
+++ guiyu-parent/guiyu-ai-parent/guiyu-ai-web/src/main/java/com/guiji/AiApplication.java
@@ -0,0 +1,22 @@
+package com.guiji.ai;
+
+import com.guiji.component.result.EnableAutoResultPack;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by ty on 2018/10/18.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoResultPack
+@EnableSwagger2
+@MapperScan("com.guiji.ai.dao")
+public class AiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class, args);
+    }
+}

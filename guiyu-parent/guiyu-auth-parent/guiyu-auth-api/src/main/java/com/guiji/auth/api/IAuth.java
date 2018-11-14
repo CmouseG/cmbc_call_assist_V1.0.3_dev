b1aebd79c961a73088b89906c8f6af36e4929a25
mayufeng <278436995@qq.com>
2018-11-14 16:51:36 +0800

    用户查询接口

--- guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IAuth.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IAuth.java
@@ -3,12 +3,12 @@ package com.guiji.auth.api;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.guiji.component.result.Result.ReturnData;
+import com.guiji.user.dao.entity.SysUser;
 
 @FeignClient("guiyu-cloud-zuul")
 public interface IAuth {
 	
-	@PostMapping(value = "/getUserId")
-	public ReturnData<Long> getUserId();
+	@PostMapping(value = "/user/getUserById")
+	public SysUser getUserById(Long userId);
 	
 }
--- guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/controller/MenuController.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/controller/MenuController.java
@@ -3,17 +3,12 @@ package com.guiji.auth.controller;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.guiji.auth.api.IAuth;
 import com.guiji.auth.service.MenuService;
-import com.guiji.component.result.Result.ReturnData;
 import com.guiji.user.dao.entity.SysMenu;
 
 @RestController
@@ -38,7 +33,7 @@ public class MenuController {
 	public void update(SysMenu menu){
 		service.update(menu);
 	}
-
+	
 	@RequestMapping("getMenuById")
 	public SysMenu getMenuById(Long id){
 		return service.getMenuById(id);

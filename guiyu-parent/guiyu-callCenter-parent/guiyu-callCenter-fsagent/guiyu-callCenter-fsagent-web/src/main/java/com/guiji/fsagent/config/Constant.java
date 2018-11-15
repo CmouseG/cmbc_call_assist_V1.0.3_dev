646086dd8aca2d36ce64c47c86731c1684478745
sunsy <sunsiyuan0510@126.com>
2018-11-15 17:16:16 +0800

    提交

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-fsagent/guiyu-callCenter-fsagent-web/src/main/java/com/guiji/fsagent/config/Constant.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-fsagent/guiyu-callCenter-fsagent-web/src/main/java/com/guiji/fsagent/config/Constant.java
@@ -10,6 +10,6 @@ public class Constant {
     public static final String SERVER_NAME_FSMANAGER = "guiyu-callcenter-fsmanager";
 
     public static final String ERROR_CODE_PARAM= "0300001";
-    public static final String ERROR_CODE_NOT_LINE= "0300002";//申请freeswitch资源失败，空闲且正常的freeswitch为空
-    public static final String ERROR_CODE_BASE64_ERROR= "0300003";//创建线路失败，线路名称重复
+    public static final String ERROR_CODE_NOT_LINE= "0300002";//更新配置信息通知请求失败，请求参数type不是line
+    public static final String ERROR_CODE_BASE64_ERROR= "0300003";//下载配置文件失败，下载新得xml后转base64过程中异常
 }

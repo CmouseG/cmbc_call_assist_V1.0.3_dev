cd5da70e686fefe894387e0d3f6234277fec1b0e
weiyunbo <weiyunbo@guiji.ai>
2018-11-15 20:34:29 +0800

    修改配置文件

---
+++ guiyu-parent/guiyu-robot-parent/guiyu-robot-web/src/main/java/com/guiji/robot/service/IAiCycleHisService.java
@@ -0,0 +1,11 @@
+package com.guiji.robot.service;
+
+/** 
+* @ClassName: IAiCycleHisService 
+* @Description: 机器人生命分配周期历史服务
+* @date 2018年11月15日 下午8:25:30 
+* @version V1.0  
+*/
+public interface IAiCycleHisService {
+
+}

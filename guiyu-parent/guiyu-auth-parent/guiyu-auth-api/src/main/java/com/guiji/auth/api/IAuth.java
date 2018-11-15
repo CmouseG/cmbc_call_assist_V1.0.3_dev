b6464092860e0b92d90fa22f89f687cf4947da18
mayufeng <278436995@qq.com>
2018-11-15 09:48:10 +0800

    用户接口

--- guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IAuth.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IAuth.java
@@ -1,7 +1,8 @@
 package com.guiji.auth.api;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.guiji.component.result.Result.ReturnData;
 import com.guiji.user.dao.entity.SysUser;
@@ -9,7 +10,7 @@ import com.guiji.user.dao.entity.SysUser;
 @FeignClient("guiyu-auth-web")
 public interface IAuth {
 	
-	@PostMapping(value = "/user/getUserById")
-	public ReturnData<SysUser> getUserById(Long userId);
+	@RequestMapping(value = "/user/getUserById")
+	public ReturnData<SysUser> getUserById(@RequestParam("userId") Long userId);
 	
 }

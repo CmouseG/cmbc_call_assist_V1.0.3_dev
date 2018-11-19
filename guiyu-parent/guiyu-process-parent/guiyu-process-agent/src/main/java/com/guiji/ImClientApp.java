254a8e910e840b604ad1a463dc00f0ce50e5ddc1
tangtq <tangtaiquan@guiji.ai>
2018-11-19 11:02:29 +0800

    进程管理

--- guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/ImClientApp.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/ImClientApp.java
@@ -2,17 +2,19 @@ package com.guiji;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
-import java.util.UUID;
 
 import com.guiji.process.agent.core.ImConnection;
 import com.guiji.process.core.message.MessageProto;
 
 import io.netty.channel.Channel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * IM 客户端启动入口
  * @author yinjihuan
  */
+@SpringBootApplication
 public class ImClientApp {
 	public static final String HOST = "127.0.0.1";
 	public static int PORT = 2222;
@@ -29,5 +31,6 @@ public class ImClientApp {
 		channel.writeAndFlush(message);*/
 		//字符串传输数据
 		//channel.writeAndFlush("yinjihuan");
+		SpringApplication.run(ImClientApp.class, args);
 	}
 }

df09bd88856b0aac01302bad51c35b6c783db867
tangtq <tangtaiquan@guiji.ai>
2018-11-20 15:52:57 +0800

    进程管理

--- guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/process/agent/timer/CheckStatusTask.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/process/agent/timer/CheckStatusTask.java
@@ -1,8 +1,12 @@
 package com.guiji.process.agent.timer;
 
 import com.guiji.process.agent.handler.ImClientProtocolBO;
-import com.guiji.process.agent.model.ProcessStatus;
 import com.guiji.process.agent.service.ProcessServiceImpl;
+import com.guiji.process.core.message.CmdMessageVO;
+import com.guiji.process.core.vo.CmdTypeEnum;
+import com.guiji.process.core.vo.DeviceStatusEnum;
+import com.guiji.process.core.vo.DeviceTypeEnum;
+import com.guiji.process.core.vo.ProcessInstanceVO;
 import com.guiji.utils.JsonUtils;
 import com.guiji.utils.StrUtils;
 import org.slf4j.Logger;
@@ -12,6 +16,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -25,20 +31,21 @@ public class CheckStatusTask {
 	
 	//定时任务，启动时运行（每1执行一次）
 	@Scheduled(fixedRate = 1000*5)
-    public void checkStatusTask() throws InterruptedException, UnsupportedEncodingException {
-		List<ProcessStatus> processStatusList = new ArrayList<ProcessStatus>();
-		for (int i=0;i<32;i++){
-			ProcessStatus processStatus = new ProcessStatus();
-			processStatus.setIp("127.0.0.1");
-			processStatus.setPort("18001");
-			String pid = processService.getPid("18001");
-			boolean isUp = processService.checkRun("18001");
-
-			processStatus.setPid(pid);
-			processStatus.setUp(isUp);
-			processStatusList.add(processStatus);
+    public void checkStatusTask() throws InterruptedException, UnsupportedEncodingException, UnknownHostException {
+		CmdMessageVO cmdMessageVO = new CmdMessageVO();
+		cmdMessageVO.setCmdType(CmdTypeEnum.HEALTH);
+		ProcessInstanceVO processInstanceVO = new ProcessInstanceVO();
+		processInstanceVO.setIp(Inet4Address.getLocalHost().getHostAddress());
+		processInstanceVO.setType(DeviceTypeEnum.SELLBOT);
+		processInstanceVO.setPort(18001);
+		boolean isUp = processService.checkRun("18001");
+		if (isUp) {
+			processInstanceVO.setStatus(DeviceStatusEnum.UP);
+		} else {
+			processInstanceVO.setStatus(DeviceStatusEnum.DOWN);
 		}
-		String msg = JsonUtils.bean2Json(processStatusList.get(0));
-		ImClientProtocolBO.getIntance().send(msg,2);
+		cmdMessageVO.setProcessInstanceVO(processInstanceVO);
+		String msg = JsonUtils.bean2Json(cmdMessageVO);
+		ImClientProtocolBO.getIntance().send(msg,1);
     }
 }
--- guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/handler/ServerPoHandlerProto.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/handler/ServerPoHandlerProto.java
@@ -1,7 +1,10 @@
 package com.guiji.process.server.handler;
 
+import com.guiji.process.core.message.CmdMessageVO;
 import com.guiji.process.core.message.MessageProto;
 import com.guiji.process.server.core.ConnectionPool;
+import com.guiji.process.server.service.impl.DeviceMsgHandler;
+import com.guiji.utils.JsonUtils;
 import com.guiji.utils.StrUtils;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -27,16 +30,10 @@ public class ServerPoHandlerProto extends ChannelInboundHandlerAdapter {
 		// ping
 		if (message.getType() == 1) {
 			System.out.println("服务端收到消息:" +  message.getContent());
-			//发送心跳包
-			MessageProto.Message.Builder builder = MessageProto.Message.newBuilder().setType(1);
-			builder.setType(1);
-			builder.setContent("服务端响应:" + "hello");
-			ctx.writeAndFlush(builder);
-		}
-
-		if (message.getType() == 2) {
-			System.out.println("服务端收到消息:" +  message.getContent());
-			//发送心跳包
+			CmdMessageVO cmdMessageVO = JsonUtils.json2Bean(message.getContent(),CmdMessageVO.class);
+			DeviceMsgHandler.getInstance().add(cmdMessageVO);
+			System.out.println("转换后的bean"+cmdMessageVO.toString());
+			//发送响应
 			MessageProto.Message.Builder builder = MessageProto.Message.newBuilder().setType(1);
 			builder.setType(1);
 			builder.setContent("服务端响应:" + "hello");

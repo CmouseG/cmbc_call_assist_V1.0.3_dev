53641ddd8e2abf8b43c8028ab0a5d9e649a4133a
tangtq <tangtaiquan@guiji.ai>
2018-11-20 11:20:26 +0800

    进程管理

--- guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/ImClientApp.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/ImClientApp.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import com.guiji.process.agent.core.ImConnection;
 import com.guiji.process.agent.core.filemonitor.impl.FileListener;
 import com.guiji.process.agent.core.filemonitor.impl.FileMonitor;
+import com.guiji.process.agent.handler.ImClientProtocolBO;
 import com.guiji.process.core.message.MessageProto;
 
 import io.netty.channel.Channel;
@@ -29,24 +30,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ImClientApp {
-	public static final String HOST = "127.0.0.1";
-	public static int PORT = 2222;
 	public static void main(String[] args) throws UnknownHostException {
-//		Channel channel = new ImConnection().connect(HOST, PORT);
-//		String id = Inet4Address.getLocalHost().getHostAddress();
-//		// protobuf
-//		MessageProto.Message message = MessageProto.Message.newBuilder().setId(id).setContent("register").build();
-//		channel.writeAndFlush(message);
-//		//对象传输数据
-//		/*Message message = new Message();
-//		message.setId(id);
-//		message.setContent("hello yinjihuan");
-//		channel.writeAndFlush(message);*/
-//		//字符串传输数据
-//		//channel.writeAndFlush("yinjihuan");
-
-		new FileMonitor().monitor("D:\\test");
-
+		ImClientProtocolBO.getIntance().start();
 		SpringApplication.run(ImClientApp.class, args);
 	}
 

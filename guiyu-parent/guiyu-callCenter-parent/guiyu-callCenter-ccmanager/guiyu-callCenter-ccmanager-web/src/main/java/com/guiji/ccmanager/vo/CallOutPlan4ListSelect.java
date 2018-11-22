1ed287d381b4741c0f5df3c0b78c49bbabbc3fd0
liyang <821927329@qq.com>
2018-11-22 10:10:56 +0800

    callcenter-ccmanager提交

---
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/vo/CallOutPlan4ListSelect.java
@@ -0,0 +1,22 @@
+package com.guiji.ccmanager.vo;
+
+import com.guiji.callcenter.dao.entity.CallOutPlan;
+
+/**
+ * @Auther: 黎阳
+ * @Date: 2018/11/22 0022 09:24
+ * @Description:
+ */
+public class CallOutPlan4ListSelect  extends CallOutPlan {
+
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}

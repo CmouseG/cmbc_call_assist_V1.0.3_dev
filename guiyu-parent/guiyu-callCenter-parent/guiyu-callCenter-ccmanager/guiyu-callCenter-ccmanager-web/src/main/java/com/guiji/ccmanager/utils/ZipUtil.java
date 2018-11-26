672302e738ec1a6c77fe706ae3dd1dbd52317719
liyang <821927329@qq.com>
2018-11-26 10:04:26 +0800

    sonarqube代码质量问题修复

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/utils/ZipUtil.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/utils/ZipUtil.java
@@ -9,15 +9,16 @@ import java.util.zip.ZipOutputStream;
 @Slf4j
 public class ZipUtil {
 
+	private ZipUtil() {
+	}
+
 	public static File zip(File source, OutputStream outresp){
 		ZipOutputStream out=null;
 		BufferedOutputStream bos=null;
 		File zip=null;
 		try {
 			log.info("compress zip start...");
-//			zip=File.createTempFile(fileName, ".zip");
 			//创建zip输出流
-//			out = new ZipOutputStream( new FileOutputStream(zip));
 			out = new ZipOutputStream( outresp);
 			//创建缓冲输出流
 			bos = new BufferedOutputStream(out);
@@ -28,26 +29,31 @@ public class ZipUtil {
 		}catch (Exception e) {
 			log.error("ZipUtil zip error:"+e);
 		}finally {
-			try{bos.close();}
-			catch (IOException e){
-				log.error("ZipUtil bos close error:"+e);
+			if (bos != null) {
+				try{bos.close();}
+				catch (IOException e){
+					log.error("ZipUtil bos close error:"+e);
+				}
 			}
 		}
 		return zip;
 	}
 
-	public static void compress(ZipOutputStream out,BufferedOutputStream bos,File sourceFile,String base) throws Exception{
+	public static void compress(ZipOutputStream out,BufferedOutputStream bos,File sourceFile,String base){
 		//如果路径为目录（文件夹）
 		if(sourceFile.isDirectory())
 		{
-
 			//取出文件夹中的文件（或子文件夹）
 			File[] flist = sourceFile.listFiles();
 
 			if(flist.length==0)//如果文件夹为空，则只需在目的地zip文件中写入一个目录进入点
 			{
 				log.info("compress file:"+base+"/");
-				out.putNextEntry(new ZipEntry(base+"/") );
+				try {
+					out.putNextEntry(new ZipEntry(base+"/") );
+				} catch (IOException e) {
+					log.error("ZipUtil compress  has error when flist.length=0:"+e);
+				}
 			}
 			else//如果文件夹不为空，则递归调用compress，文件夹中的每一个文件（或文件夹）进行压缩
 			{
@@ -58,21 +64,38 @@ public class ZipUtil {
 			}
 		}
 		else{//如果不是目录（文件夹），即为文件，则先写入目录进入点，之后将文件写入zip文件中
-			out.putNextEntry( new ZipEntry(base) );
-			FileInputStream fos = new FileInputStream(sourceFile);
-			BufferedInputStream bis = new BufferedInputStream(fos);
+			BufferedInputStream bis = null;
+			FileInputStream fos = null;
+			try{
+				out.putNextEntry( new ZipEntry(base) );
+				fos = new FileInputStream(sourceFile);
+				bis = new BufferedInputStream(fos);
 
-			int tag;
-			log.info("compress file:"+base);
-			//将源文件写入到zip文件中
-			while((tag=bis.read())!=-1){
-				bos.write(tag);
+				int tag;
+				log.info("compress file:"+base);
+				//将源文件写入到zip文件中
+				while((tag=bis.read())!=-1){
+					bos.write(tag);
+				}
+				bos.flush();
+
+			}catch (IOException e){
+				log.error("ZipUtil compress has error:"+e);
+			}finally {
+				if(bis!=null){
+					try{bis.close();}
+					catch (IOException e){
+						log.error("ZipUtil compress close bis error:"+e);
+					}
+				}
+				if(fos!=null){
+					try{fos.close();}
+					catch (IOException e){
+						log.error("ZipUtil compress close fos error:"+e);
+					}
+				}
 			}
-			bos.flush();
-			try{bis.close();}
-			catch (IOException e){
-				log.error("ZipUtil compress close error:"+e);
-			}
+
 		}
 	}
 	

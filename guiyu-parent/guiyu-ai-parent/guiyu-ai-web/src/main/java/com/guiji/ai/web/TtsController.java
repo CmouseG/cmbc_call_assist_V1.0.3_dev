81c08f295d0730d729d6ad5686198b416e69fb14
MrSun931205 <867677564@qq.com>
2018-12-03 10:11:29 +0800

    TTS异步处理修改

--- guiyu-parent/guiyu-ai-parent/guiyu-ai-web/src/main/java/com/guiji/ai/web/TtsController.java
+++ guiyu-parent/guiyu-ai-parent/guiyu-ai-web/src/main/java/com/guiji/ai/web/TtsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.guiji.ai.api.ITts;
 import com.guiji.ai.tts.TtsReqQueue;
+import com.guiji.ai.tts.constants.AiConstants;
 import com.guiji.ai.vo.TtsReqVO;
 import com.guiji.component.result.Result;
 import com.guiji.component.result.Result.ReturnData;
@@ -16,23 +17,28 @@ import com.guiji.component.result.Result.ReturnData;
  * 语音合成 Created by ty on 2018/11/13.
  */
 @RestController
-public class TtsController implements ITts {
+public class TtsController implements ITts
+{
 	private static Logger logger = LoggerFactory.getLogger(TtsController.class);
 
-	
 	@Override
 	@PostMapping(value = "translate")
-	public ReturnData<String> translate(@RequestBody TtsReqVO ttsReqVO) {
-
-		try {
-			if (ttsReqVO != null && !ttsReqVO.getContents().isEmpty()) 
+	public ReturnData<String> translate(@RequestBody TtsReqVO ttsReqVO)
+	{
+		try
+		{
+			if (ttsReqVO != null && !ttsReqVO.getContents().isEmpty())
 			{
 				TtsReqQueue.getInstance().produce(ttsReqVO);
 			}
-
-		} catch (Exception e) {
-			logger.error("", e);
+			
+			return Result.ok("success");
+			
+		} catch (Exception e)
+		{
+			logger.error("请求失败！", e);
+			return Result.error(AiConstants.AI_REQUEST);
 		}
-		return Result.ok("success");
+		
 	}
 }

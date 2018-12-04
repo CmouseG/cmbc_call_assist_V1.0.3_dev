4d7b722441c933c8bd88a4862f6ce5df11805dbb
tangtq <tangtaiquan@guiji.ai>
2018-12-04 13:18:35 +0800

    进程管理sql

--- sql/guiyu_base.sql
+++ sql/guiyu_base.sql
@@ -159,16 +159,17 @@ CREATE TABLE `sys_process` (
   `type` int(10) DEFAULT NULL COMMENT '进程类型：0:TTS 1:SELLBOT 99:AGENT ',
   `process_key` varchar(255) DEFAULT NULL COMMENT '扩展字段，type为TTS时存模型名称',
   `status` int(2) DEFAULT NULL COMMENT '状态0:UP1:DOWN2:BUSY3:MISSING',
+  `exec_status` int(2) DEFAULT NULL COMMENT '执行状态0执行完成1执行中',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1170 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for sys_process_log
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_process_log`;
-CREATE TABLE `sys_process_log` (
+CREATE TABLE `sys_process_task` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `process_id` bigint(20) DEFAULT NULL,
   `ip` varchar(100) DEFAULT NULL,
@@ -178,10 +179,15 @@ CREATE TABLE `sys_process_log` (
   `parameters` varchar(255) DEFAULT NULL COMMENT '命令参数',
   `result` varchar(255) DEFAULT NULL COMMENT '执行结果',
   `result_content` varchar(255) DEFAULT NULL,
+  `exec_status` int(2) DEFAULT NULL COMMENT '执行状态:0执行结束1执行中',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
+  `create_by` bigint(20) DEFAULT NULL COMMENT '创建人',
+  `update_by` bigint(20) DEFAULT NULL COMMENT '更新人',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
 
 -- ----------------------------
 -- Table structure for sys_role

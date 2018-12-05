c51c110c47b9ca2bcb68c772c3253ae54cdfa5af
mayufeng <278436995@qq.com>
2018-12-05 16:27:53 +0800

    添加创建操作人创建时间

---
+++ guiyu-parent/guiyu-botstence-parent/guiyu-botstence-web/src/main/java/ai/guiji/botsentence/controller/BotPublishSentenceLogController.java
@@ -0,0 +1,39 @@
+package ai.guiji.botsentence.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ai.guiji.botsentence.dao.BotPublishSentenceLogMapper;
+import ai.guiji.botsentence.dao.entity.BotPublishSentenceLog;
+import ai.guiji.botsentence.dao.entity.BotPublishSentenceLogExample;
+import ai.guiji.component.client.config.JsonParam;
+import ai.guiji.component.model.Page;
+import ai.guiji.component.model.ServerResult;
+
+@RestController
+@RequestMapping("publishSentence")
+public class BotPublishSentenceLogController {
+	
+	@Autowired
+	private BotPublishSentenceLogMapper botPublishSentenceLogMapper;
+	
+	@RequestMapping("log")
+	public ServerResult getBotPublishSentenceLogByPage(@JsonParam int pageSize,@JsonParam int pageNo){
+		BotPublishSentenceLogExample example=new BotPublishSentenceLogExample();
+		example.setLimitStart((pageSize-1)*pageNo);
+		example.setLimitEnd(pageSize*pageNo);
+		List<BotPublishSentenceLog>  results =botPublishSentenceLogMapper.selectByExample(example);
+		
+		Page<BotPublishSentenceLog> page = new Page<BotPublishSentenceLog>();
+		int totalNum=botPublishSentenceLogMapper.countByExample(example);
+		page.setPageNo(pageNo);
+		page.setPageSize(pageSize);
+		page.setRecords(results);
+		page.setTotal(totalNum);
+		
+		return ServerResult.createBySuccess(page);
+	}
+}

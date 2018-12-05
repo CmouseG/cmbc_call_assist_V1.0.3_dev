bf4fdf7bdaa7304ef20e2797f5fc3c7d1745df74
mayufeng <278436995@qq.com>
2018-12-05 13:26:58 +0800

    bug修复

---
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-dal/src/main/java/com/guiji/user/vo/MenuParamVo.java
@@ -0,0 +1,47 @@
+package com.guiji.user.vo;
+
+public class MenuParamVo {
+
+	private int pageNo;
+	
+	private int totalPage;
+	
+	private int pageSize = 20;
+	
+	private String menuName="";
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	
+	
+}

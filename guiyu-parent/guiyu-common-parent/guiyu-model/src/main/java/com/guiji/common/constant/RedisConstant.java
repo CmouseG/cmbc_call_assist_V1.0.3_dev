e29c5f8322edf5ba75ac618193b3652c0831a2b7
tangtq <tangtaiquan@guiji.ai>
2018-12-05 19:30:53 +0800

    进程管理

--- guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/constant/RedisConstant.java
+++ guiyu-parent/guiyu-common-parent/guiyu-model/src/main/java/com/guiji/common/constant/RedisConstant.java
@@ -6,5 +6,5 @@ package com.guiji.common.constant;
 public class RedisConstant {
     public static final String REDIS_DICT_PREFIX = "SYS_DICT_KEY_";
     public static final String REDIS_PROCESS_TASK_PREFIX = "SYS_PROCESS_TASK_";
-    public static final Long REDIS_PROCESS_TASK_EXPIRE = 300000L;
+    public static final Long REDIS_PROCESS_TASK_EXPIRE = 1L;
 }

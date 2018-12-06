84d98f0598940ec3b09d55c51b34f432efe672b4
tangtq <tangtaiquan@guiji.ai>
2018-12-06 11:36:58 +0800

    进程管理

--- guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/controller/ProcessController.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/controller/ProcessController.java
@@ -6,10 +6,7 @@ import com.guiji.process.server.dao.entity.SysProcess;
 import com.guiji.process.server.model.ProcessCmdVO;
 import com.guiji.process.server.service.ISysProcessService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class ProcessController {
     @Autowired
     ISysProcessService sysProcessService;
 
-    @PostMapping("/list")
+    @GetMapping("/list")
     public Page<SysProcess> list(int pageNo, int pageSize, SysProcess sysProcess) {
         return sysProcessService.queryProcessPage(pageNo,pageSize,sysProcess);
     }

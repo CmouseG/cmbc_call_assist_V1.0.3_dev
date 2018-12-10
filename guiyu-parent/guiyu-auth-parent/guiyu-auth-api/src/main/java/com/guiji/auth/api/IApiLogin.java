5b81216c2b011bb47a344f86e0331251034a2179
mayufeng <278436995@qq.com>
2018-12-10 16:15:32 +0800

    api登录

---
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IApiLogin.java
@@ -0,0 +1,18 @@
+package com.guiji.auth.api;
+
+import java.util.List;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.guiji.component.result.Result.ReturnData;
+import com.guiji.user.dao.entity.SysRole;
+
+@FeignClient("guiyu-cloud-zuul")
+public interface IApiLogin {
+	
+	@RequestMapping("apiLogin")
+	public ReturnData<List<SysRole>> getRoleByUserId(@RequestParam("userId") Long userId);
+	
+}

83a7dfa6b2f75db8d948a7675a0e5baccbb48cd6
liyang <821927329@qq.com>
2018-12-10 15:07:20 +0800

    新建的2张统计表

---
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-dal/src/main/java/com/guiji/callcenter/dao/entity/ReportCallToday.java
@@ -0,0 +1,84 @@
+package com.guiji.callcenter.dao.entity;
+
+import java.io.Serializable;
+
+public class ReportCallToday implements Serializable {
+    private Long id;
+
+    private Integer durationType;
+
+    private String intent;
+
+    private String reason;
+
+    private Integer callCount;
+
+    private Long durationAll;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getDurationType() {
+        return durationType;
+    }
+
+    public void setDurationType(Integer durationType) {
+        this.durationType = durationType;
+    }
+
+    public String getIntent() {
+        return intent;
+    }
+
+    public void setIntent(String intent) {
+        this.intent = intent == null ? null : intent.trim();
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason == null ? null : reason.trim();
+    }
+
+    public Integer getCallCount() {
+        return callCount;
+    }
+
+    public void setCallCount(Integer callCount) {
+        this.callCount = callCount;
+    }
+
+    public Long getDurationAll() {
+        return durationAll;
+    }
+
+    public void setDurationAll(Long durationAll) {
+        this.durationAll = durationAll;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", durationType=").append(durationType);
+        sb.append(", intent=").append(intent);
+        sb.append(", reason=").append(reason);
+        sb.append(", callCount=").append(callCount);
+        sb.append(", durationAll=").append(durationAll);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}

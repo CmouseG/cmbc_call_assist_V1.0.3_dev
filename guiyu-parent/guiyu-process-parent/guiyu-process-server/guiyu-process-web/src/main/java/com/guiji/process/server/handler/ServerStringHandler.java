ece5bc90b9ceacc58ff70a87e0866d4c9d81282a
tangtq <tangtaiquan@guiji.ai>
2018-12-11 15:29:27 +0800

    进程管理

--- guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/handler/ServerStringHandler.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/handler/ServerStringHandler.java
@@ -1,5 +1,6 @@
 package com.guiji.process.server.handler;
 
+import com.guiji.process.core.message.CmdProtoMessage;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -12,8 +13,8 @@ public class ServerStringHandler extends ChannelInboundHandlerAdapter {
 	
 	@Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		System.err.println("server:" + msg.toString());
-		ctx.writeAndFlush(msg.toString() + "你好");
+        CmdProtoMessage.ProtoMessage message = (CmdProtoMessage.ProtoMessage) msg;
+		System.err.println("server:" + message.getContent());
     }
 
     @Override
--- guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/service/impl/ProcessServerCmdHandler.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-server/guiyu-process-web/src/main/java/com/guiji/process/server/service/impl/ProcessServerCmdHandler.java
@@ -13,8 +13,10 @@ import com.guiji.process.core.vo.CmdTypeEnum;
 import com.guiji.common.model.process.ProcessInstanceVO;
 import com.guiji.process.server.dao.entity.SysProcess;
 import com.guiji.process.server.dao.entity.SysProcessTask;
+import com.guiji.process.server.model.ProcessTask;
 import com.guiji.process.server.service.ISysProcessTaskService;
 import com.guiji.process.server.service.ISysProcessService;
+import com.guiji.utils.IdGenUtil;
 import com.guiji.utils.JsonUtils;
 import com.guiji.utils.RedisUtil;
 import org.slf4j.Logger;
@@ -25,6 +27,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProcessServerCmdHandler implements IProcessCmdHandler {
@@ -66,15 +69,15 @@ public class ProcessServerCmdHandler implements IProcessCmdHandler {
                 doUnRegister(cmdMessageVO);
                 break;
             case RESTART:
-                processManageService.cmd(cmdMessageVO.getProcessInstanceVO(), CmdTypeEnum.RESTART, cmdMessageVO.getParameters(),cmdMessageVO.getUserId());
+                processManageService.cmd(cmdMessageVO.getProcessInstanceVO(), CmdTypeEnum.RESTART, cmdMessageVO.getParameters(),cmdMessageVO.getUserId(), IdGenUtil.uuid());
                 break;
             case UNKNOWN:
                 break;
             case START:
-                processManageService.cmd(cmdMessageVO.getProcessInstanceVO(), CmdTypeEnum.START, cmdMessageVO.getParameters(),cmdMessageVO.getUserId());
+                processManageService.cmd(cmdMessageVO.getProcessInstanceVO(), CmdTypeEnum.START, cmdMessageVO.getParameters(),cmdMessageVO.getUserId(), IdGenUtil.uuid());
                 break;
             case STOP:
-                processManageService.cmd(cmdMessageVO.getProcessInstanceVO(), CmdTypeEnum.STOP, cmdMessageVO.getParameters(),cmdMessageVO.getUserId());
+                processManageService.cmd(cmdMessageVO.getProcessInstanceVO(), CmdTypeEnum.STOP, cmdMessageVO.getParameters(),cmdMessageVO.getUserId(), IdGenUtil.uuid());
                 break;
             case HEALTH:
                 doHealthStatus(cmdMessageVO);
@@ -164,24 +167,65 @@ public class ProcessServerCmdHandler implements IProcessCmdHandler {
         if (cmdMessageVO != null) {
             ProcessInstanceVO processInstanceVO = cmdMessageVO.getProcessInstanceVO();
             if (processInstanceVO != null){
-                //发布MQ通知消息
-                String tmplId =  cmdMessageVO.getParameters().get(1);
-                PublishBotstenceResultMsgVO publishBotstenceResultMsgVO = new PublishBotstenceResultMsgVO();
-                if (cmdMessageVO.getCmdType() == CmdTypeEnum.PULBLISH_SELLBOT_BOTSTENCE) {
-                    publishBotstenceResultMsgVO.setProcessTypeEnum(ProcessTypeEnum.SELLBOT);
-                } else if (cmdMessageVO.getCmdType() == CmdTypeEnum.PULBLISH_FREESWITCH_BOTSTENCE) {
-                    publishBotstenceResultMsgVO.setProcessTypeEnum(ProcessTypeEnum.FREESWITCH);
-                }else if (cmdMessageVO.getCmdType() == CmdTypeEnum.PUBLISH_ROBOT_BOTSTENCE) {
-                    publishBotstenceResultMsgVO.setProcessTypeEnum(ProcessTypeEnum.ROBOT);
+                List<String> jobs = (List<String>)redisUtil.get("GY_PROCESS_JOB");
+                if (jobs != null) {
+                    for (String jobId : jobs) {
+                        Map<String,ProcessTask> processTaskMap = (Map<String,ProcessTask>)redisUtil.get(jobId);
+                        if (processTaskMap != null) {
+                            for (Map.Entry<String, ProcessTask> entry: processTaskMap.entrySet()) {
+                                if (cmdMessageVO.getReqKey() != null && cmdMessageVO.getReqKey().equals(entry.getKey())) {
+                                    entry.getValue().setResult(cmdMessageVO.getCommandResult());
+                                    processTaskMap.put(entry.getKey(),entry.getValue());
+                                    break;
+                                }
+                            }
+                            redisUtil.set(jobId,processTaskMap);
+                        }
+                    }
                 }
 
-                publishBotstenceResultMsgVO.setTmplId(tmplId);
-                if (cmdMessageVO.getCommandResult() != null) {
-                    publishBotstenceResultMsgVO.setResult(Integer.valueOf(cmdMessageVO.getCommandResult()));
+                boolean sendMQ = true;
+
+                if (jobs != null) {
+                    for (String jobId : jobs) {
+                        Map<String,ProcessTask> processTaskMap = (Map<String,ProcessTask>)redisUtil.get(jobId);
+                        if (processTaskMap != null) {
+                            for (Map.Entry<String, ProcessTask> entry: processTaskMap.entrySet()) {
+                                if (cmdMessageVO.getReqKey() != null && cmdMessageVO.getReqKey().equals(entry.getKey())) {
+                                    ProcessTask processTask = entry.getValue();
+                                    if (!"0".equals(processTask.getResult()) && processTask.getRetryCount() < 3) {
+                                        sendMQ = false;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
 
-                fanoutSender.send("fanoutPublishBotstence", JsonUtils.bean2Json(publishBotstenceResultMsgVO));
+                if (sendMQ) {
+                    //发布MQ通知消息
+                    String tmplId =  cmdMessageVO.getParameters().get(1);
+                    PublishBotstenceResultMsgVO publishBotstenceResultMsgVO = new PublishBotstenceResultMsgVO();
+                    if (cmdMessageVO.getCmdType() == CmdTypeEnum.PULBLISH_SELLBOT_BOTSTENCE) {
+                        publishBotstenceResultMsgVO.setProcessTypeEnum(ProcessTypeEnum.SELLBOT);
+                    } else if (cmdMessageVO.getCmdType() == CmdTypeEnum.PULBLISH_FREESWITCH_BOTSTENCE) {
+                        publishBotstenceResultMsgVO.setProcessTypeEnum(ProcessTypeEnum.FREESWITCH);
+                    }else if (cmdMessageVO.getCmdType() == CmdTypeEnum.PUBLISH_ROBOT_BOTSTENCE) {
+                        publishBotstenceResultMsgVO.setProcessTypeEnum(ProcessTypeEnum.ROBOT);
+                    }
 
+                    publishBotstenceResultMsgVO.setTmplId(tmplId);
+                    if (cmdMessageVO.getCommandResult() != null) {
+                        if (Integer.valueOf(cmdMessageVO.getCommandResult()) == 0) {
+                            publishBotstenceResultMsgVO.setResult(0);
+                        } else {
+                            publishBotstenceResultMsgVO.setResult(1);
+                        }
+
+                    }
+                    fanoutSender.send("fanoutPublishBotstence", JsonUtils.bean2Json(publishBotstenceResultMsgVO));
+                }
                 //更新sys_process_task
                 SysProcessTask sysProcessTask = new SysProcessTask();
                 sysProcessTask.setResult(cmdMessageVO.getCommandResult());

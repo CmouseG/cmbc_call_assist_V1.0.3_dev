f98f77d8fe4a0a10401a9be6a350ab7cb8500da8
MrSun931205 <867677564@qq.com>
2018-12-12 10:59:35 +0800

    sysoperalog

--- guiyu-parent/guiyu-common-parent/guiyu-sysoperalog-parent/guiyu-sysoperalog-web/src/main/java/com/guiji/sysoperalog/SysOperaLogApplication.java
+++ guiyu-parent/guiyu-common-parent/guiyu-sysoperalog-parent/guiyu-sysoperalog-web/src/main/java/com/guiji/sysoperalog/SysOperaLogApplication.java
@@ -13,10 +13,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableSwagger2
 @MapperScan("com.guiji.guiyu.sysoperalog.dao")
-public class Application
+public class SysOperaLogApplication
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(Application.class);
+		SpringApplication.run(SysOperaLogApplication.class);
 	}
 }

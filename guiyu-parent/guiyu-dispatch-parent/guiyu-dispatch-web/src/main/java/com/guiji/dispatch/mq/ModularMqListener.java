2d33164c4bd4031b41fcc1523e4394ecdcfe192f
xujin <728805437@qq.com>
2018-12-12 15:04:07 +0800

    add

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/mq/ModularMqListener.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/mq/ModularMqListener.java
@@ -1,7 +1,5 @@
 package com.guiji.dispatch.mq;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;

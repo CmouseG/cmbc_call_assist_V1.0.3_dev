d59d9bab919d08e201689df4beb063ac33f760c2
tangtq <tangtaiquan@guiji.ai>
2018-12-13 17:29:01 +0800

    数据字典增加用户操作日志注解

--- guiyu-parent/guiyu-common-parent/guiyu-datadic/guiyu-datadic-web/src/main/java/com/guiji/dict/controller/SysDictLocalController.java
+++ guiyu-parent/guiyu-common-parent/guiyu-datadic/guiyu-datadic-web/src/main/java/com/guiji/dict/controller/SysDictLocalController.java
@@ -1,6 +1,7 @@
 package com.guiji.dict.controller;
 
 import com.guiji.common.model.Page;
+import com.guiji.component.aspect.SysOperaLog;
 import com.guiji.dict.dao.entity.SysDict;
 import com.guiji.dict.service.SysDictService;
 import io.swagger.annotations.ApiOperation;
@@ -24,30 +25,35 @@ public class SysDictLocalController {
     @Autowired
     private SysDictService sysDictService;
 
+    @SysOperaLog(operaTarget = "数据字典", operaType = "新增/修改")
     @ApiOperation(value="新增/更新字典信息", notes="根据是否有ID新增或者更新字典信息")
     @RequestMapping(value = "/saveOrUpdateDict", method = RequestMethod.POST)
     public SysDict saveOrUpdateDict(SysDict sysDict,@RequestHeader Long userId) {
         return sysDictService.saveOrUpdateDict(sysDict,userId);
     }
 
+    @SysOperaLog(operaTarget = "数据字典", operaType = "查询")
     @ApiOperation(value="查询字典信息", notes="根据查询条件查询字典信息")
     @RequestMapping(value = "/queryDictList", method = RequestMethod.POST)
     public List<SysDict> queryDictList(SysDict sysDict) {
         return sysDictService.queryDictList(sysDict);
     }
 
+    @SysOperaLog(operaTarget = "数据字典", operaType = "分页查询")
     @ApiOperation(value="分页字典信息", notes="根据查询条件分页查询字典信息")
     @RequestMapping(value = "/queryDictPage", method = RequestMethod.POST)
     public Page<SysDict> queryDictPage(int pageNo, int pageSize, SysDict sysDict) {
         return sysDictService.queryDictPage(pageNo, pageSize, sysDict);
     }
 
+    @SysOperaLog(operaTarget = "数据字典", operaType = "根据ID查询字典信息")
     @ApiOperation(value="获取字典信息", notes="根据ID查询字典信息")
     @RequestMapping(value = "/get", method = RequestMethod.POST)
     public SysDict get(Long id) {
         return sysDictService.get(id);
     }
 
+    @SysOperaLog(operaTarget = "数据字典", operaType = "删除")
     @ApiOperation(value="删除字典信息", notes="根据ID删除字典信息")
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public int delete(Long id) {

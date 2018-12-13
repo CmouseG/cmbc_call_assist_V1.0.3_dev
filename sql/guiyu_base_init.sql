531c2b55e1d6f2a0f34c517aed1032bf2b8fbb15
mayufeng <278436995@qq.com>
2018-12-13 13:06:00 +0800

    初始化数据

--- sql/guiyu_base_init.sql
+++ sql/guiyu_base_init.sql
@@ -67,14 +67,3 @@ delete from `sys_role_user`;
 INSERT INTO `sys_role_user` (`user_id`, `role_id`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`) VALUES ('1', '1', '1', now(), '1', now(), '0');
 
 
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_branch', 1, 1, 'BRN', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_domain', 1, 1, 'DOM', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_intent', 1, 1, 'INT', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_label', 1, 1, 'XXX', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_process', 1, 1, 'PRO', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_template', 1, 1, 'TEM', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_tts_backup', 1, 1, 'BAK', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('bot_sentence_tts_param', 1, 1, 'PARAM', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('sys_user', 1, 1, 'USER', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('user_account', 1, 1, 'ACC', null);
-INSERT INTO bd_table_sequence (table_name, seq, step, SIGN, now_date) VALUES ('user_account_industry_relation', 1, 1, 'RELA', '20181012');

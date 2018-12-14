963f1a876767dc344ab4d84913d30dd4f83c2204
xujin <728805437@qq.com>
2018-12-14 14:18:38 +0800

    增加记录logs的功能

---
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/Constant.java
@@ -0,0 +1,16 @@
+package com.guiji.dispatch.model;
+
+public class Constant {
+	
+	//任务状态
+	public static final Integer MODULAR_STATUS_START = 0;
+	public static final Integer MODULAR_STATUS_END = 1;
+	public static final Integer MODULAR_STATUS_ERROR = 2;
+	
+	//模块名称
+	public static final Integer MODULAR_DISPATCH = 0;
+	public static final Integer MODULAR_CALLCENTER = 1;
+	public static final Integer  MODULAR_ROBOT = 2;
+	public static final Integer  MODULAR_TTS = 3;
+	
+}

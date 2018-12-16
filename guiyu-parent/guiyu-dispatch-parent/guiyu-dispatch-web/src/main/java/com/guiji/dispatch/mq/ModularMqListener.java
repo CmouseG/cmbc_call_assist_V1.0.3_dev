36cbbe4d202eb46e330e075aa1b48e95a1255534
xujin <728805437@qq.com>
2018-12-16 14:51:46 +0800

    解决logs记录的问题

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/mq/ModularMqListener.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/mq/ModularMqListener.java
@@ -1,9 +1,11 @@
 package com.guiji.dispatch.mq;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import com.guiji.dispatch.dao.entity.DispatchPlan;
 import com.guiji.dispatch.dao.entity.DispatchPlanExample;
 import com.guiji.dispatch.dao.entity.ModularLogs;
 import com.guiji.utils.JsonUtils;
+import com.rabbitmq.client.Channel;
 
 
 @Component
@@ -29,18 +32,27 @@ public class ModularMqListener {
 	private DispatchPlanMapper dispatchPlanMapper;
 
 	@RabbitHandler
-	public void process(String message) {
-		ModularLogs logs = JsonUtils.json2Bean(message, ModularLogs.class);
-		DispatchPlanExample ex = new DispatchPlanExample();
-		ex.createCriteria().andPlanUuidEqualTo(logs.getPlanUuid());
-		 List<DispatchPlan> selectByExample = dispatchPlanMapper.selectByExample(ex);
-		 DispatchPlan dispatchPlan = null;
-		 if(selectByExample.size()>0){
-			 dispatchPlan = selectByExample.get(0);
-		 }else{
-			 logger.info("接受MQ消息有问题了");
-		 }
-		logs.setBatchName(dispatchPlan.getBatchName());
-		modularLogsMapper.insert(logs);
+	public void process(String message,Channel channel,Message message2) {
+		try {
+			ModularLogs logs = JsonUtils.json2Bean(message, ModularLogs.class);
+			DispatchPlanExample ex = new DispatchPlanExample();
+			ex.createCriteria().andPlanUuidEqualTo(logs.getPlanUuid());
+			List<DispatchPlan> selectByExample = dispatchPlanMapper.selectByExample(ex);
+			DispatchPlan dispatchPlan = null;
+			if (selectByExample.size() > 0) {
+				dispatchPlan = selectByExample.get(0);
+			} else {
+				logger.info("接受MQ消息有问题了");
+			}
+			logs.setBatchName(dispatchPlan.getBatchName());
+			modularLogsMapper.insert(logs);
+		} catch (Exception e) {
+			//这次消息，我已经接受并消费掉了，不会再重复发送消费
+			try {
+				channel.basicAck(message2.getMessageProperties().getDeliveryTag(), false);
+			} catch (IOException e1) {
+				logger.error("已经接受并消费掉了，不会再重复发送消费有问题了");
+			}
+		}
 	}
 }

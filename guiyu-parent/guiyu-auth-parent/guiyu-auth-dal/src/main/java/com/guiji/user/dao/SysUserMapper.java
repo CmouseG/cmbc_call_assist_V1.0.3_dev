14f1a2b4fedc6b6e36f579225e93d998f9065ce3
mayufeng <278436995@qq.com>
2018-12-18 15:31:46 +0800

    用户信息修改更新 Signed-off-by: mayufeng <278436995@qq.com>

--- guiyu-parent/guiyu-auth-parent/guiyu-auth-dal/src/main/java/com/guiji/user/dao/SysUserMapper.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-dal/src/main/java/com/guiji/user/dao/SysUserMapper.java
@@ -36,7 +36,7 @@ public interface SysUserMapper {
     int updateByPrimaryKey(SysUser record);
     
     //
-    void addRole(@Param("userId")Long userId,@Param("roleIds")String[] roleIds);
+    void addRole(@Param("userId")Long userId,@Param("roleIds")Long roleIds);
     
     Set<String> getPermissions(String principal);
     

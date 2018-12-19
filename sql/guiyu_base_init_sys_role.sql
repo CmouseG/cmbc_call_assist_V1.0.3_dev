2eaf27c774f4d5fa4eb370ef99e2991f289e8bb7
lizhong <lizhong@btows.com>
2018-12-19 15:26:55 +0800

    初始化权限组数据

---
+++ sql/guiyu_base_init_sys_role.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `guiyu_base`.`sys_role`(`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES (1, '管理员', '管理员', 1, '2018-12-13 05:12:06', 1, '2018-12-17 14:05:27', '0', 0, 0);
+INSERT INTO `guiyu_base`.`sys_role`(`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES (2, '代理商', '代理商', 1, '2018-12-13 15:07:44', 1, '2018-12-18 19:18:10', '0', 0, 1);
+INSERT INTO `guiyu_base`.`sys_role`(`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES (3, '企业管理员', '企业管理员', 1, '2018-12-13 15:35:49', 1, '2018-12-18 19:18:35', '0', 0, 1);
+INSERT INTO `guiyu_base`.`sys_role`(`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES (4, '企业操作员', '企业操作员', 1, '2018-12-17 14:05:06', 1, '2018-12-18 19:19:06', '0', 0, 1);

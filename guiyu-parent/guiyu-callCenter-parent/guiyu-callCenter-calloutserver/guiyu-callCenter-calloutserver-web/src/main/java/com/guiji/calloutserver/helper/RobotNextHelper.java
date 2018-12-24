71ba7b82668fc30ab0471a736a0954eea877a31f
liyang <821927329@qq.com>
2018-12-24 21:31:46 +0800

    推送日志修改

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/helper/RobotNextHelper.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/helper/RobotNextHelper.java
@@ -120,7 +120,7 @@ public class RobotNextHelper {
                             dealWithResponse(aiResponse);
                         }
                     } catch (Exception e) {
-                        log.error("scheduledExecutorService.scheduleAtFixedRate has error: callId[{}]:",e, callId);
+                        log.error("scheduledExecutorService.scheduleAtFixedRate has error: ",e);
                     }
                 },
                 0, 500, TimeUnit.MILLISECONDS);

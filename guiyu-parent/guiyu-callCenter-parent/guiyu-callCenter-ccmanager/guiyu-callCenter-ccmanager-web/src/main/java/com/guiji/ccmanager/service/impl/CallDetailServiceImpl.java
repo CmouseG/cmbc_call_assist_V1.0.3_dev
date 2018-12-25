0e90387f5cd25236e15e8d908d1018de8dd146fd
liyang <821927329@qq.com>
2018-12-25 19:37:38 +0800

    57环境，企业管理员，通话记录导出不正确

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/CallDetailServiceImpl.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/CallDetailServiceImpl.java
@@ -273,7 +273,7 @@ public class CallDetailServiceImpl implements CallDetailService {
         CallOutDetailExample example = new CallOutDetailExample();
         CallOutDetailExample.Criteria criteria = example.createCriteria();
         criteria.andCallIdEqualTo(callId);
-        example.setOrderByClause("bot_answer_time");
+        example.setOrderByClause("IF(ISNULL(bot_answer_time),customer_say_time,bot_answer_time)");
         List<CallOutDetail> list = callOutDetailMapper.selectByExample(example);
         String result = "";
         if (list != null && list.size() > 0) {
@@ -291,7 +291,7 @@ public class CallDetailServiceImpl implements CallDetailService {
         CallOutDetailExample example = new CallOutDetailExample();
         String[] callidArr = callIds.split(",");
         example.createCriteria().andCallIdIn(Arrays.asList(callidArr));
-        example.setOrderByClause("call_id,bot_answer_time");
+        example.setOrderByClause("call_id,IF(ISNULL(bot_answer_time),customer_say_time,bot_answer_time)");
 
         List<CallOutDetail> list = callOutDetailMapper.selectByExample(example);
 

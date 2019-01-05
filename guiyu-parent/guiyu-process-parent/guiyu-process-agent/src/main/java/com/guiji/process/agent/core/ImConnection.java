57fd55eecbf293b0452ef455a3931cee33fcd2c8
tangtq <tangtaiquan@guiji.ai>
2019-01-05 19:02:42 +0800

    DB整改-进程管理

--- guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/process/agent/core/ImConnection.java
+++ guiyu-parent/guiyu-process-parent/guiyu-process-agent/src/main/java/com/guiji/process/agent/core/ImConnection.java
@@ -76,9 +76,6 @@ public class ImConnection {
 			ChannelFuture f = b.connect(host, port);
 			f.addListener(new ConnectionListener());
 			channel = f.channel();
-			if (ImClientProtocolBO.channelGlobal != null) {
-				ImClientProtocolBO.channelGlobal.close();
-			}
 			ImClientProtocolBO.channelGlobal = channel;
 		} catch(Exception e) {
 			e.printStackTrace();

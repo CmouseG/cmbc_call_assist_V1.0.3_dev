6ddc42483512fcb9e7d4bc68e9bcd06fda100ef4
xujin <728805437@qq.com>
2019-01-16 13:38:28 +0800

    提交dto代码

---
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/bean/PlanUserIdLineRobotDto.java
@@ -0,0 +1,29 @@
+package com.guiji.dispatch.bean;
+
+public class PlanUserIdLineRobotDto {
+	private Integer userId;
+	private Integer lineId;
+	private String robot ;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getLineId() {
+		return lineId;
+	}
+	public void setLineId(Integer lineId) {
+		this.lineId = lineId;
+	}
+	public String getRobot() {
+		return robot;
+	}
+	public void setRobot(String robot) {
+		this.robot = robot;
+	}
+	
+	
+	
+	
+}

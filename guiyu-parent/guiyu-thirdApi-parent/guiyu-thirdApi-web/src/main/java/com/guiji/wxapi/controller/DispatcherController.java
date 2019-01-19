e879838775ae2ae985da2eb7f0119e1e574e3e87
liyang <821927329@qq.com>
2019-01-19 17:29:32 +0800

    新增调度2个接口

--- guiyu-parent/guiyu-thirdApi-parent/guiyu-thirdApi-web/src/main/java/com/guiji/wxapi/controller/DispatcherController.java
+++ guiyu-parent/guiyu-thirdApi-parent/guiyu-thirdApi-web/src/main/java/com/guiji/wxapi/controller/DispatcherController.java
@@ -1,25 +1,39 @@
 package com.guiji.wxapi.controller;
 
 import com.guiji.component.result.Result;
+import com.guiji.dispatch.api.IDispatchPlanOut;
+import com.guiji.dispatch.model.PlanCountVO;
 import io.swagger.annotations.ApiOperation;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 public class DispatcherController {
 
+    @Autowired
+    IDispatchPlanOut dispatchPlanOut;
+
     @ApiOperation(value = "获取计划数")
     @GetMapping("5c2f306a2393c")
-    public Result.ReturnData<String[]> getCallCount(@RequestHeader Long userId) {
-        return null;
+    public Result.ReturnData<PlanCountVO> getCallCount(@RequestHeader String orgCode) {
+
+        return dispatchPlanOut.getPlanCountByUserId(orgCode);
+
     }
 
 
-    @ApiOperation(value = "一键停止拨打")
+    @ApiOperation(value = "一键停止拨打,type： 1包括，0 不包括")
     @GetMapping("5c2f30ceb5cde")
-    public Result.ReturnData<String[]> stopCallPlan(@RequestHeader Long userId) {
-        return null;
+    public Result.ReturnData<Boolean> stopCallPlan(@RequestHeader("orgCode") String orgCode,
+                                                   @RequestParam("type") @NotEmpty(message = "type不能为空") String type) {
+
+        return dispatchPlanOut.opertationStopPlanByUserId(orgCode,type);
     }
 
 

32004a249b61ca8bcb546622cb43db1c48f2db7a
liyang <821927329@qq.com>
2019-01-19 11:41:59 +0800

    修改第三方接口

--- guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IApiLogin.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-api/src/main/java/com/guiji/auth/api/IApiLogin.java
@@ -2,13 +2,13 @@ package com.guiji.auth.api;
 
 import com.guiji.user.dao.entity.SysUser;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("guiyu-auth-web")
 public interface IApiLogin {
 	
-	@RequestMapping("getUserByAccess")
-	public SysUser getUserByAccess(@RequestParam("accessKey")String accessKey, @RequestParam("secretKey")String secretKey);
+	@GetMapping("getUserByAccess")
+	SysUser getUserByAccess(@RequestParam("accessKey")String accessKey, @RequestParam("secretKey")String secretKey);
 	
 }
--- guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/controller/ApiLoginController.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/controller/ApiLoginController.java
@@ -4,14 +4,19 @@ import com.guiji.auth.api.IApiLogin;
 import com.guiji.auth.service.UserService;
 import com.guiji.user.dao.entity.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ApiLoginController implements IApiLogin {
 
     @Autowired
     private UserService service;
 
     @Override
-    public SysUser getUserByAccess(String accessKey, String secretKey) {
+    @GetMapping("getUserByAccess")
+    public SysUser getUserByAccess(@RequestParam("accessKey")String accessKey, @RequestParam("secretKey")String secretKey) {
 
         SysUser sysUser =  service.getUserByAccess(accessKey, secretKey);
         return sysUser;

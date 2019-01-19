d5e28056db92eb4eb0f3b111a50fd64f5fd202c6
xujin <728805437@qq.com>
2019-01-19 17:49:38 +0800

    增加第三方接口逻辑

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/PlanResultInfo.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/model/PlanResultInfo.java
@@ -3,6 +3,14 @@ package com.guiji.dispatch.model;
 public class PlanResultInfo {
 	private int succCount;
 	private int errorCount;
+	private String msg="";
+	
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
 	public int getSuccCount() {
 		return succCount;
 	}

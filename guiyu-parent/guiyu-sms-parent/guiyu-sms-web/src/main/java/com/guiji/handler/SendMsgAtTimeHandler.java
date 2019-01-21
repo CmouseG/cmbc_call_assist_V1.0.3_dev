9c48aac3356657c92fe3e31384cc0827ab440060
孙东波 <867677564@qq.com>
2019-01-21 16:05:58 +0800

    Sms定时任务

--- guiyu-parent/guiyu-sms-parent/guiyu-sms-web/src/main/java/com/guiji/handler/SendMsgAtTimeHandler.java
+++ guiyu-parent/guiyu-sms-parent/guiyu-sms-web/src/main/java/com/guiji/handler/SendMsgAtTimeHandler.java
@@ -2,10 +2,7 @@ package com.guiji.handler;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.guiji.model.TaskReq;
@@ -14,38 +11,41 @@ import com.guiji.service.TaskService;
 import com.guiji.sms.dao.entity.SmsTask;
 import com.guiji.utils.ListUtil;
 import com.guiji.utils.RedisUtil;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import com.xxl.job.core.log.XxlJobLogger;
 
+@JobHandler(value="SendMsgAtTimeHandler")
 @Component
-public class SendMsgAtTimeHandler
+public class SendMsgAtTimeHandler extends IJobHandler
 {
-	private static final Logger logger = LoggerFactory.getLogger(SendMsgAtTimeHandler.class);
-	
 	@Autowired
 	RedisUtil redisUtil;
 	@Autowired
 	TaskService taskService;
 	@Autowired
 	SendSmsService sendSmsService;
-	
-	@Scheduled(fixedRate = 1000*60*1) //每分钟执行一次
-	public void task() throws Exception
+
+	@Override
+	public ReturnT<String> execute(String param) throws Exception
 	{
 		List<SmsTask> taskList = taskService.getTimeTaskList();
 		if(taskList == null || taskList.isEmpty()){
-			logger.info("没有可执行的短信发送任务");
-			return;
+			XxlJobLogger.log("没有可执行的短信发送任务");
+			return SUCCESS;
 		}
 		
 		for(SmsTask task : taskList)
 		{
 			if(task.getAuditingStatus() == 0)
 			{
-				logger.info("短信内容未审核，暂不能发送！");
+				XxlJobLogger.log("短信内容未审核，暂不能发送！");
 				continue;
 			}
 			if(task.getRunStatus() == 0)
 			{
-				logger.info("短信任务已停止，暂不发送！");
+				XxlJobLogger.log("短信任务已停止，暂不发送！");
 				continue;
 			}
 			List<Object> phoneList = redisUtil.lGet(task.getTaskName(), 0, -1);
@@ -55,7 +55,7 @@ public class SendMsgAtTimeHandler
 			taskReq.setSendTime(task.getSendDate());
 			sendSmsService.sendMessages(taskReq); //群发短信
 		}
-		
+		return SUCCESS;
 	}
 
 }

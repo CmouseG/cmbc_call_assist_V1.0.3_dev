9d259945ab87f1f0a443ebf8f6f9668f7284ce07
liyang <821927329@qq.com>
2019-01-22 11:55:14 +0800

    Update guiyu_callCenter.sql

--- sql/sql追加/1.3版本/guiyu_callCenter.sql
+++ sql/sql追加/1.3版本/guiyu_callCenter.sql
@@ -36,3 +36,22 @@ CREATE TABLE `report_line_status` (
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE  `call_out_detail` ADD COLUMN isupdate INT(1) DEFAULT 0 COMMENT '是否修改过';
+ALTER TABLE  `call_out_detail_0` ADD COLUMN isupdate INT(1) DEFAULT 0 COMMENT '是否修改过';
+ALTER TABLE  `call_out_detail_1` ADD COLUMN isupdate INT(1) DEFAULT 0 COMMENT '是否修改过';
+
+DROP TABLE IF EXISTS `call_out_detail_log`;
+
+CREATE TABLE `call_out_detail_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `call_detail_id` bigint(20) DEFAULT NULL,
+  `customer_say_text_new` varchar(255) DEFAULT NULL,
+  `customer_say_text` varchar(255) DEFAULT NULL,
+  `update_by` int(8) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+

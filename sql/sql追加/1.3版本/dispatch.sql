43f54e5ce2cf5217144494f8b8581bc72c8a501b
xujin <728805437@qq.com>
2019-01-22 16:27:13 +0800

    整改sql类型和大小

--- sql/sql追加/1.3版本/dispatch.sql
+++ sql/sql追加/1.3版本/dispatch.sql
@@ -3,12 +3,11 @@ ALTER TABLE  `file_error_records` ADD COLUMN data_type INT(11) DEFAULT 0 COMMENT
 ALTER TABLE  `file_error_records` ADD COLUMN batch_id INT(11) DEFAULT 0 COMMENT '批次id';
 ALTER TABLE  `file_error_records` ADD COLUMN batch_name varchar(32) DEFAULT 0 COMMENT '批次名字';
 
-DROP TABLE IF EXISTS `third_interface_records`;
 CREATE TABLE `third_interface_records` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `create_time` date DEFAULT NULL,
-  `url` varchar(1024) DEFAULT NULL,
-  `params` varchar(15000) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `params` varchar(2048) DEFAULT NULL,
   `times` int(32) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `type` int(11) DEFAULT NULL COMMENT '1成功 0失败',

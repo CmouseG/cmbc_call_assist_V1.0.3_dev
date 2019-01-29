5c12d11c845ab76e891e4e68be97eef56deb5aa1
xujin <728805437@qq.com>
2019-01-29 14:08:45 +0800

    修改sql长度

--- sql/sql追加/1.4版本/dispatch.sql
+++ sql/sql追加/1.4版本/dispatch.sql
@@ -2,8 +2,8 @@ use guiyu_dispatch;
 ALTER TABLE  `black_list` ADD COLUMN user_id INT(11) DEFAULT 0 COMMENT '用户id';
 ALTER TABLE  `black_list` ADD COLUMN update_user_id INT(11) DEFAULT 0 COMMENT '修改userid';
 ALTER TABLE  `black_list` ADD COLUMN org_code varchar(8) DEFAULT 0 COMMENT '批次名字';
-ALTER TABLE  `black_list` ADD COLUMN create_user_name varchar(8) DEFAULT 0 COMMENT '创建用户名';
-ALTER TABLE  `black_list` ADD COLUMN update_user_name varchar(8) DEFAULT 0 COMMENT '修改用户名字';
+ALTER TABLE  `black_list` ADD COLUMN create_user_name varchar(32) DEFAULT 0 COMMENT '创建用户名';
+ALTER TABLE  `black_list` ADD COLUMN update_user_name varchar(32) DEFAULT 0 COMMENT '修改用户名字';
 ALTER TABLE  `black_list` ADD COLUMN status varchar(8) DEFAULT 0 COMMENT '0未删除1删除';
 
 

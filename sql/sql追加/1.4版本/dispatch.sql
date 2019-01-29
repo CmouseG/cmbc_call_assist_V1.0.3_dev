1323213737a06a26fa835e89373378cecacdab2c
xujin <728805437@qq.com>
2019-01-29 17:39:37 +0800

    增加黑名单导入记录

--- sql/sql追加/1.4版本/dispatch.sql
+++ sql/sql追加/1.4版本/dispatch.sql
@@ -1,4 +1,16 @@
 use guiyu_dispatch;
+
+CREATE TABLE `black_list_records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(32) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_name` varchar(32) DEFAULT NULL,
+  `org_code` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
 ALTER TABLE  `black_list` ADD COLUMN user_id INT(11) DEFAULT 0 COMMENT '用户id';
 ALTER TABLE  `black_list` ADD COLUMN update_user_id INT(11) DEFAULT 0 COMMENT '修改userid';
 ALTER TABLE  `black_list` ADD COLUMN org_code varchar(8) DEFAULT 0 COMMENT '批次名字';

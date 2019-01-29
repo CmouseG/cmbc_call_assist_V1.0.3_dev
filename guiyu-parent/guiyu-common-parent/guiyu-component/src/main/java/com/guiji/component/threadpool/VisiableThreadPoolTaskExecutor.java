b7f065a4d141780ddab2dc5e6d23d164409fe726
xujin <728805437@qq.com>
2019-01-29 17:37:42 +0800

    增加线程池公共类

--- guiyu-parent/guiyu-common-parent/guiyu-component/src/main/java/com/guiji/component/threadpool/VisiableThreadPoolTaskExecutor.java
+++ guiyu-parent/guiyu-common-parent/guiyu-component/src/main/java/com/guiji/component/threadpool/VisiableThreadPoolTaskExecutor.java
@@ -14,6 +14,29 @@ public class VisiableThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
 
     private static Logger logger = LoggerFactory.getLogger(VisiableThreadPoolTaskExecutor.class);
 
+    public VisiableThreadPoolTaskExecutor(String prefixName, int corePoolSize, int maxPoolSize, int capacity, int waitSeconds) {
+        super();
+
+        //设置最小线程数
+        super.setCorePoolSize(corePoolSize);
+        //设置最大线程数
+        super.setMaxPoolSize(maxPoolSize);
+        //等待队列
+        super.setQueueCapacity(capacity);
+        // 等待时间 （默认为0，此时立即停止），并没等待xx秒后强制停止
+        super.setAwaitTerminationSeconds(waitSeconds);
+        //  线程名称前缀
+        super.setThreadNamePrefix(prefixName);
+
+        //等待任务在关机时完成--表明等待所有线程执行完
+        super.setWaitForTasksToCompleteOnShutdown(true);
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
+        super.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 初始化线程
+        super.initialize();
+    }
+
     private void showThreadPoolInfo(String prefix) {
         ThreadPoolExecutor threadPoolExecutor = getThreadPoolExecutor();
 
@@ -21,7 +44,7 @@ public class VisiableThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
             return;
         }
 
-        logger.info("{}, {},taskCount [{}], completedTaskCount [{}], activeCount [{}], queueSize [{}]",
+        logger.debug("{}, {},taskCount [{}], completedTaskCount [{}], activeCount [{}], queueSize [{}]",
                 this.getThreadNamePrefix(),
                 prefix,
                 threadPoolExecutor.getTaskCount(),

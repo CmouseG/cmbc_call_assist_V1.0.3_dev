d28fa5abdaba87f7a8b6fd36623fe1093dc60943
liyang <821927329@qq.com>
2019-01-30 21:54:37 +0800

    关于分段录音问题提交

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/eventbus/event/AsrCustomerEvent.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/eventbus/event/AsrCustomerEvent.java
@@ -1,7 +1,13 @@
 package com.guiji.calloutserver.eventbus.event;
 
+import com.guiji.callcenter.dao.entity.CallOutPlan;
+import lombok.Data;
+
 /**
  * ASR识别事件
  */
+@Data
 public class AsrCustomerEvent extends AsrBaseEvent{
+
+    private CallOutPlan callPlan;
 }

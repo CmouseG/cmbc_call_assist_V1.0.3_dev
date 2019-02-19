c13a9b15c126c089dce105c93fc613de8b8fc2a0
qianxin <qixiworld@163.com>
2019-02-19 14:44:23 +0800

    计费实时通知欠费消息

--- guiyu-parent/guiyu-billing-parent/guiyu-billing-web/src/main/java/com/guiji/billing/service/impl/ChargingServiceImpl.java
+++ guiyu-parent/guiyu-billing-parent/guiyu-billing-web/src/main/java/com/guiji/billing/service/impl/ChargingServiceImpl.java
@@ -2,13 +2,16 @@ package com.guiji.billing.service.impl;
 
 import com.guiji.auth.api.IAuth;
 import com.guiji.billing.constants.BusiTypeEnum;
+import com.guiji.billing.dao.mapper.ext.BillingChargingMapper;
 import com.guiji.billing.dao.mapper.ext.BillingUserAcctMapper;
 import com.guiji.billing.dto.CallChargingNotifyDto;
+import com.guiji.billing.entity.BillNotifyLogBean;
 import com.guiji.billing.entity.BillingAcctChargingRecord;
 import com.guiji.billing.entity.BillingAcctChargingTerm;
 import com.guiji.billing.entity.BillingUserAcctBean;
 import com.guiji.billing.enums.*;
 import com.guiji.billing.exception.BaseException;
+import com.guiji.billing.service.AcctNotifyService;
 import com.guiji.billing.service.ChargingService;
 import com.guiji.billing.utils.DaoHandler;
 import com.guiji.billing.utils.IdWorker;
@@ -16,10 +19,15 @@ import com.guiji.billing.utils.ResHandler;
 import com.guiji.component.result.Result;
 import com.guiji.user.dao.entity.SysOrganization;
 import com.guiji.user.dao.entity.SysUser;
+import com.guiji.vo.ArrearageNotifyVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -29,11 +37,19 @@ import java.util.List;
 @Service
 public class ChargingServiceImpl implements ChargingService {
 
+    private Logger logger = LoggerFactory.getLogger(ChargingServiceImpl.class);
+
     @Autowired
     private IAuth iAuth;
 
     @Autowired
     private BillingUserAcctMapper billingUserAcctMapper;
+
+    @Autowired
+    private BillingChargingMapper billingChargingMapper;
+
+    @Autowired
+    private AcctNotifyService acctNotifyService;
 
     @Autowired
     private IdWorker idWorker;
@@ -47,6 +63,9 @@ public class ChargingServiceImpl implements ChargingService {
     public boolean charging(CallChargingNotifyDto callChargingNotifyDto) {
         boolean bool = false;
         if (null != callChargingNotifyDto){
+            //新增话单日志
+            this.addBillNotifyLog(callChargingNotifyDto);
+
             Integer userId = callChargingNotifyDto.getUserId();//用户ID
             Integer billSec = callChargingNotifyDto.getBillSec();//通话时长 秒
             String lineId = callChargingNotifyDto.getLineId()+"";   //通话线路
@@ -57,58 +76,59 @@ public class ChargingServiceImpl implements ChargingService {
             SysUser user = ResHandler.getResObj(iAuth.getUserById(Long.valueOf(userId)));
             //查询企业组织
             SysOrganization org = ResHandler.getResObj(iAuth.getOrgByUserId(Long.valueOf(userId)));
-            if(null != user && null != org){
-                String orgCode = org.getCode();
-                //查询企业用户账户
-                BillingUserAcctBean acct = billingUserAcctMapper.queryUserAcctByOrgCode(orgCode);
-                if(null != acct) {
-                    String accountId = acct.getAccountId();
-                    BillingAcctChargingTerm acctChargingParam = new BillingAcctChargingTerm();
-                    acctChargingParam.setAccountId(accountId);
-                    acctChargingParam.setUserId(userId+"");
-                    acctChargingParam.setChargingItemId(lineId);
-                    List<BillingAcctChargingTerm> termList = billingUserAcctMapper.queryAcctChargingTermList(acctChargingParam, null);
-                    if(null != termList && termList.size()>0) {
-                        BillingAcctChargingTerm term = termList.get(0);
-                        //通话时长
-                        Long duration = Long.valueOf(billSec)   ;
-                        //此次通话消费金额
-                        BigDecimal consumeAmount = this.getAmount(term, duration);
-                        //账户可用金额
-                        BigDecimal availableBalance = acct.getAvailableBalance();
-                        //消费前账户金额
-                        BigDecimal srcAmount = availableBalance;
-                        //消费后账户金额
-                        BigDecimal toAmount = availableBalance.subtract(consumeAmount);
+            String orgCode = (null != org)?org.getCode():null;
+            //查询企业用户账户
+            BillingUserAcctBean acct = billingUserAcctMapper.queryUserAcctByOrgCode(orgCode);
+            String accountId = null != acct?acct.getAccountId():null;
+            BillingAcctChargingTerm acctChargingParam = new BillingAcctChargingTerm();
+            acctChargingParam.setAccountId(accountId);
+            acctChargingParam.setUserId(userId+"");
+            acctChargingParam.setChargingItemId(lineId);
+            List<BillingAcctChargingTerm> termList = billingUserAcctMapper.queryAcctChargingTermList(acctChargingParam, null);
+            if(null != termList && termList.size()>0) {
+                BillingAcctChargingTerm term = termList.get(0);
+                //通话时长
+                Long duration = Long.valueOf(billSec)   ;
+                //此次通话消费金额
+                BigDecimal consumeAmount = this.getAmount(term, duration);
+                //账户可用金额
+                BigDecimal availableBalance = null != acct?acct.getAvailableBalance():BigDecimal.ZERO;
+                //消费前账户金额
+                BigDecimal srcAmount = availableBalance;
+                //消费后账户金额
+                BigDecimal toAmount = availableBalance.subtract(consumeAmount);
 
-                        BillingAcctChargingRecord chargingRecord = new BillingAcctChargingRecord();
-                        chargingRecord.setChargingId(idWorker.getBusiId(BusiTypeEnum.BILLING_ACCT.getType()));
-                        chargingRecord.setAccountId(accountId);
-                        chargingRecord.setOperUserId(userId+"");
-                        chargingRecord.setOperUserName(user.getUsername());
-                        chargingRecord.setOperUserOrgCode(user.getOrgCode());
-                        chargingRecord.setOperBeginTime(beginTime);
-                        chargingRecord.setOperEndTime(endTime);
-                        chargingRecord.setOperDuration(duration);
-                        chargingRecord.setOperDurationM(this.getMins(duration));
-                        chargingRecord.setOperDurationStr(this.getDurationStr(duration));
+                BillingAcctChargingRecord chargingRecord = new BillingAcctChargingRecord();
+                chargingRecord.setChargingId(idWorker.getBusiId(BusiTypeEnum.BILLING_ACCT.getType()));
+                chargingRecord.setAccountId(accountId);
+                chargingRecord.setOperUserId(userId+"");
+                chargingRecord.setOperUserName(null != user?user.getUsername():"");
+                chargingRecord.setOperUserOrgCode(null != user?user.getOrgCode():"");
+                chargingRecord.setOperBeginTime(beginTime);
+                chargingRecord.setOperEndTime(endTime);
+                chargingRecord.setOperDuration(duration);
+                chargingRecord.setOperDurationM(this.getMins(duration));
+                chargingRecord.setOperDurationStr(this.getDurationStr(duration));
 
-                        chargingRecord.setType(ChargingTypeEnum.CONSUME.getType());//消费
-                        chargingRecord.setFeeMode(AcctChargingFeeModeEnum.CALL_RESUME.getFeeCode());//通话消费
-                        chargingRecord.setUserChargingId(term.getUserChargingId());
-                        chargingRecord.setAmount(consumeAmount);
-                        chargingRecord.setSrcAmount(srcAmount);
-                        chargingRecord.setToAmount(toAmount);
-                        chargingRecord.setPhone(phone);
-                        chargingRecord.setCreateTime(new Date());
-                        chargingRecord.setDelFlag(SysDelEnum.NORMAL.getState());
-                        //插入计费记录
-                        bool = DaoHandler.getMapperBoolRes(billingUserAcctMapper.addAcctChargingRecord(chargingRecord));
-                        if(bool) {
-                            //计费扣减企业账户余额
-                            bool = DaoHandler.getMapperBoolRes(billingUserAcctMapper.subAcctBalanceAmount(accountId, consumeAmount, new Date()));
-                        }
-                    }
+                chargingRecord.setType(ChargingTypeEnum.CONSUME.getType());//消费
+                chargingRecord.setFeeMode(AcctChargingFeeModeEnum.CALL_RESUME.getFeeCode());//通话消费
+                chargingRecord.setUserChargingId(term.getUserChargingId());
+                chargingRecord.setAmount(consumeAmount);
+                chargingRecord.setSrcAmount(srcAmount);
+                chargingRecord.setToAmount(toAmount);
+                chargingRecord.setPhone(phone);
+                chargingRecord.setCreateTime(new Date());
+                chargingRecord.setDelFlag(SysDelEnum.NORMAL.getState());
+                //插入计费记录
+                bool = DaoHandler.getMapperBoolRes(billingUserAcctMapper.addAcctChargingRecord(chargingRecord));
+                if(bool) {
+                    //计费扣减企业账户余额
+                    bool = DaoHandler.getMapperBoolRes(billingUserAcctMapper.subAcctBalanceAmount(accountId, consumeAmount, new Date()));
+                }
+
+                //如果扣费后欠费，则欠费消息通知
+                if(toAmount.compareTo(BigDecimal.ZERO)<=0){
+                    this.notifyArrearage(String.valueOf(userId));
                 }
             }
             return bool;
@@ -116,6 +136,29 @@ public class ChargingServiceImpl implements ChargingService {
             throw new BaseException(SysDefaultExceptionEnum.NULL_PARAM_EXCEPTION.getErrorCode(),
                     SysDefaultExceptionEnum.NULL_PARAM_EXCEPTION.getErrorMsg());
         }
+    }
+
+    /**
+     * 欠费消息通知
+     * @param userId
+     */
+    private void notifyArrearage(String userId){
+        logger.info("用户ID:{}欠费通知开始", userId);
+        try {
+            //查询欠费企业的用户列表
+            ArrearageNotifyVo arrearage = new ArrearageNotifyVo();
+            List<String> userIdList = new ArrayList<String>();
+            userIdList.add(userId);
+            //欠费用户ID
+            arrearage.setUserIdList(userIdList);
+            //欠费状态
+            arrearage.setIsArrearage(AcctArrearageStatusEnum.ARREARAGE.getStatus());
+            //通知欠费消息
+            acctNotifyService.notifyArrearage(arrearage);
+        }catch(Exception e){
+            logger.error("用户ID:{"+userId+"}欠费通知异常", e);
+        }
+        logger.info("用户ID:{}欠费通知结束", userId);
     }
 
     /**
@@ -168,5 +211,21 @@ public class ChargingServiceImpl implements ChargingService {
             str = "00:" + (duration>9?duration:("0"+duration));
         }
         return str;
+    }
+
+    /**
+     * 新增话单日志
+     * @param callChargingNotifyDto
+     */
+    private void addBillNotifyLog(CallChargingNotifyDto callChargingNotifyDto){
+        if(null != callChargingNotifyDto) {
+            try {
+                BillNotifyLogBean billLog = new BillNotifyLogBean();
+                BeanUtils.copyProperties(callChargingNotifyDto, billLog, BillNotifyLogBean.class);
+                billingChargingMapper.addBillNotifyLog(billLog);
+            }catch(Exception e){
+                logger.error("新增话单日志异常", e);
+            }
+        }
     }
 }

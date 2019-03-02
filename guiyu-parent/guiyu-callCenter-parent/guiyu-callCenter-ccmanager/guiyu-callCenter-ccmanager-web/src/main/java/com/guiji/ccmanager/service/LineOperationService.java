576cd08201efe70cb3f5450166255cd67b47f563
liyang <821927329@qq.com>
2019-03-02 18:37:33 +0800

    批量新增线路接口

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/LineOperationService.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/LineOperationService.java
@@ -2,8 +2,14 @@ package com.guiji.ccmanager.service;
 
 import com.guiji.ccmanager.entity.OutLineInfoAddReq;
 import com.guiji.ccmanager.entity.OutLineInfoUpdateReq;
+import com.guiji.fsmanager.entity.LineInfoVO;
+
+import java.util.List;
 
 public interface LineOperationService {
+
+    List<LineInfoVO> batchAddLineInfo(List<OutLineInfoAddReq> outLineInfoAddReqList);
+
     Integer addLineInfo(OutLineInfoAddReq outLineInfoAddReq);
 
     void updateLineInfo(OutLineInfoUpdateReq outLineInfoUpdateReq);

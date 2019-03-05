b44fdf412d542e737ecd2dae87b2f1c2761b916c
liyang <821927329@qq.com>
2019-03-05 11:08:21 +0800

    批量操作线路

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-api/src/main/java/com/guiji/ccmanager/api/ILineOperation.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-api/src/main/java/com/guiji/ccmanager/api/ILineOperation.java
@@ -3,12 +3,15 @@ package com.guiji.ccmanager.api;
 import com.guiji.ccmanager.entity.OutLineInfoAddReq;
 import com.guiji.ccmanager.entity.OutLineInfoUpdateReq;
 import com.guiji.component.result.Result;
+import com.guiji.fsmanager.entity.LineInfoVO;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient("guiyu-callcenter-ccmanager")
 public interface ILineOperation {
@@ -24,4 +27,9 @@ public interface ILineOperation {
     @ApiOperation(value = "删除线路,呼叫中心的线路id")
     @GetMapping(value = "/out/deleteLineInfo")
     Result.ReturnData deleteLineInfo(@RequestParam(value = "LineId") Integer LineId);
+
+    @ApiOperation(value = "批量新增线路")
+    @PostMapping(value = "/out/batchAddLineInfo")
+    Result.ReturnData<List<LineInfoVO>> batchAddLineInfo(@RequestBody List<OutLineInfoAddReq> outLineInfoAddReqList);
+
 }

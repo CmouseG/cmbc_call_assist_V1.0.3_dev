4728ab4cb90dade841c55d3975f3a5b9920bfdc4
tangtq <tangtaiquan@guiji.ai>
2019-03-08 22:18:23 +0800

    开户管理bug修改

--- guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/service/OrganizationService.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/service/OrganizationService.java
@@ -93,7 +93,7 @@ public class OrganizationService {
 		for(Map map : list)
 		{
 			ServerResult<Integer> result = botSentenceProcess.countTemplateByOrgCode((String) map.get("code"));
-			if(result != null && "0".equals(result.getRspCode())) {
+			if(result != null && "000000".equals(result.getRspCode())) {
 				map.put("botstence", result.getData());
 			}
 		}

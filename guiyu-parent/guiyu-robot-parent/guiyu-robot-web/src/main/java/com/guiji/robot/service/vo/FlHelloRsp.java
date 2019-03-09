9196ea4a233d1d45d59fa281bdd1907fced45b6a
weiyunbo <weiyunbo@guiji.ai>
2019-03-09 09:52:23 +0800

    飞龙返回增加字段

--- guiyu-parent/guiyu-robot-parent/guiyu-robot-web/src/main/java/com/guiji/robot/service/vo/FlHelloRsp.java
+++ guiyu-parent/guiyu-robot-parent/guiyu-robot-web/src/main/java/com/guiji/robot/service/vo/FlHelloRsp.java
@@ -13,6 +13,10 @@ import lombok.Data;
 public class FlHelloRsp {
 	//会话id
 	private String seqid;
+	//话术模板
+	private String cfg_name;
+	//子话术模板
+	private String sub_cfg_name;
 	//当前域
 	private String answered_domain;
 	//意图级别
@@ -23,8 +27,16 @@ public class FlHelloRsp {
 	private String sentence;
 	//原因
 	private String reason;
+	//命中场景或者说域
+	private String scene_name;
 	//是否结束（0表示正常继续，1表示会话结束，网关进行挂断操作，2表示转人工）
 	private String end;
 	//要播放的录音文件ID
 	private String wav_filename;
+	//机器人返回状态
+	private String status;
+	//意图
+	private String intent_name;
+	//客户信息
+	private String userinfo;
 }

eb7ec58b31c2a481680d71119c99fae04dac80bd
tangtaiquan <tangtaiquan@guiji.ai>
2019-03-13 16:17:06 +0800

    Upload new file

---
+++ sql/sql初始化/初始化语句/guiyu_base/sys_organization_product_init.sql
@@ -0,0 +1,2 @@
+use guiyu_base;
+INSERT INTO `guiyu_base`.`sys_organization_product` (`id`, `organization_id`, `product`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`) VALUES ('1', '1', '0', '3', '2019-03-08 11:42:23', '3', '2019-03-08 11:42:23', '0');

ec0913a250329f50a4cb1a48ce6d2dc46f400d84
tangtaiquan <tangtaiquan@guiji.ai>
2019-03-13 16:17:15 +0800

    Upload new file

---
+++ sql/sql初始化/初始化语句/guiyu_base/sys_role_init.sql
@@ -0,0 +1,6 @@
+use guiyu_base;
+INSERT INTO `guiyu_base`.`sys_role` (`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES ('1', '管理员', '管理员', '1', '2018-12-13 05:12:06', '13', '2019-03-01 00:12:26', '0', '0', '0');
+INSERT INTO `guiyu_base`.`sys_role` (`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES ('2', '代理商', '代理商', '1', '2018-12-13 15:07:44', '54', '2019-03-09 12:17:04', '0', '0', '1');
+INSERT INTO `guiyu_base`.`sys_role` (`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES ('3', '企业管理员', '企业管理员', '1', '2018-12-13 15:35:49', '3', '2019-03-07 22:58:50', '0', '0', '1');
+INSERT INTO `guiyu_base`.`sys_role` (`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES ('4', '企业操作员', '企业操作员', '1', '2018-12-17 14:05:06', '54', '2019-03-11 10:50:07', '0', '0', '1');
+INSERT INTO `guiyu_base`.`sys_role` (`id`, `name`, `desc`, `create_id`, `create_time`, `update_id`, `update_time`, `del_flag`, `init_role`, `super_admin`) VALUES ('5', '企业客服', '企业客服', '1', '2018-12-17 14:05:06', '54', '2019-03-11 10:45:26', '0', '0', '1');

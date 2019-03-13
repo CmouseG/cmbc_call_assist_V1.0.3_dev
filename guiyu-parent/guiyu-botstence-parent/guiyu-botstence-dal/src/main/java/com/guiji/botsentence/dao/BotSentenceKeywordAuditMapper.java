6a9aa1e5654d668298f53d31f16683e9f639d559
demon7452 <demonli7452@gmail.com>
2019-03-13 10:16:30 +0800

    dao and mapper

---
+++ guiyu-parent/guiyu-botstence-parent/guiyu-botstence-dal/src/main/java/com/guiji/botsentence/dao/BotSentenceKeywordAuditMapper.java
@@ -0,0 +1,31 @@
+package com.guiji.botsentence.dao;
+
+import com.guiji.botsentence.dao.entity.BotSentenceKeywordAudit;
+import com.guiji.botsentence.dao.entity.BotSentenceKeywordAuditExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BotSentenceKeywordAuditMapper {
+    long countByExample(BotSentenceKeywordAuditExample example);
+
+    int deleteByExample(BotSentenceKeywordAuditExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(BotSentenceKeywordAudit record);
+
+    int insertSelective(BotSentenceKeywordAudit record);
+
+    List<BotSentenceKeywordAudit> selectByExample(BotSentenceKeywordAuditExample example);
+
+    BotSentenceKeywordAudit selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") BotSentenceKeywordAudit record, @Param("example") BotSentenceKeywordAuditExample example);
+
+    int updateByExample(@Param("record") BotSentenceKeywordAudit record, @Param("example") BotSentenceKeywordAuditExample example);
+
+    int updateByPrimaryKeySelective(BotSentenceKeywordAudit record);
+
+    int updateByPrimaryKey(BotSentenceKeywordAudit record);
+}

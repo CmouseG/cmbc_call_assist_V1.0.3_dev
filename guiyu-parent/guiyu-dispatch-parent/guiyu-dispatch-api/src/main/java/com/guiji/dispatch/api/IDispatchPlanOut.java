ec569b8e63f7d67257736243dd14898d44ed96da
qianxin <qixiworld@163.com>
2019-03-14 11:03:57 +0800

    接口地址

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/api/IDispatchPlanOut.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-api/src/main/java/com/guiji/dispatch/api/IDispatchPlanOut.java
@@ -135,11 +135,11 @@ public interface IDispatchPlanOut {
 
     //查询计划任务
     @ApiOperation(value="根据uuId查询计划任务")
-    @RequestMapping(value="/dispatch/api/queryDispatchPlanById", method={RequestMethod.GET})
+    @RequestMapping(value="/dispatch/queryDispatchPlanById", method={RequestMethod.GET})
     Result.ReturnData<DispatchPlan> queryDispatchPlanById(@RequestParam("planUuid") String planUuid);
 
    @ApiOperation(value="根据uuId查询计划任务备注")
-   @RequestMapping(value="/dispatch/api/queryPlanRemarkById", method={RequestMethod.GET})
+   @RequestMapping(value="/dispatch/queryPlanRemarkById", method={RequestMethod.GET})
    Result.ReturnData<String> queryPlanRemarkById(@RequestParam("planUuid") String planUuid);
 
 }

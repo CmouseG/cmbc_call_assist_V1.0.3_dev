a673ac421dc6a8f20419dbf7dfc4963f53a7f01e
liyang <821927329@qq.com>
2019-03-14 17:32:29 +0800

    修复问题

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/LineNameServiceImpl.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/LineNameServiceImpl.java
@@ -5,6 +5,7 @@ import com.guiji.clm.api.LineMarketRemote;
 import com.guiji.clm.model.SipLineVO;
 import com.guiji.component.result.Result;
 import com.guiji.utils.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
  * Date:2019/3/14 16:15
  * Description:
  */
+@Slf4j
 @Service
 public class LineNameServiceImpl implements LineNameService {
 
@@ -30,12 +32,21 @@ public class LineNameServiceImpl implements LineNameService {
             return (String) o;
         }else{
             //请求线路市场获取线路名称
-            Result.ReturnData<SipLineVO> result = lineMarketRemote.queryUserSipLineByLineId(String.valueOf(customerId),lineId);
-            String lineName = result.getBody().getLineName();
-            redisUtil.set("ccmanger_lineName_"+lineId+"_"+customerId,lineName);
-            return lineName;
-        }
+            try{
+                Result.ReturnData<SipLineVO> result = lineMarketRemote.queryUserSipLineByLineId(String.valueOf(customerId),lineId);
+                log.info("queryUserSipLineByLineId返回:",result.getCode());
+                if(result.success){
+                    String lineName = result.getBody().getLineName();
+                    redisUtil.set("ccmanger_lineName_"+lineId+"_"+customerId,lineName);
+                    return lineName;
+                }
+            }catch (Exception e){
+                log.error("获取线路名称出现异常,",e);
+                return "";
+            }
 
+        }
+        return "";
     }
 
 }

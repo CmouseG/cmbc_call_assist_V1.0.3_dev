639ed8216802c2ccb9b0977f3705d43fca3bcf58
zhangpeng <zhangpeng@guiji.ai>
2019-03-20 17:33:03 +0800

    话术市场发布任务表字段扩大

--- sql/sql追加/20190321/guiyu_botstence.sql
+++ sql/sql追加/20190321/guiyu_botstence.sql
@@ -3,9 +3,9 @@ CREATE TABLE
     bot_sentence_deploy
     (
         id bigint NOT NULL AUTO_INCREMENT comment '主键id',
-        job_id VARCHAR(32) comment '发布任务ID',
-        sub_job_id VARCHAR(32) comment '发布子任务ID',
-        process_id VARCHAR(32) comment '话术流程编号',
+        job_id VARCHAR(256) comment '发布任务ID',
+        sub_job_id VARCHAR(256) comment '发布子任务ID',
+        process_id VARCHAR(256) comment '话术流程编号',
         template_id VARCHAR(32) comment '话术模板编号',
         status VARCHAR(32) comment '子任务部署状态',
         crt_time DATETIME comment '创建时间',

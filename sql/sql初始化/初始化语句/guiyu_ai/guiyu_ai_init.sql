648f8da759c93e43b02dfb1d79e2f1ce29f2b934
孙东波 <867677564@qq.com>
2019-03-20 10:09:03 +0800

    ai初始化数据

--- sql/sql初始化/初始化语句/guiyu_ai/guiyu_ai_init.sql
+++ sql/sql初始化/初始化语句/guiyu_ai/guiyu_ai_init.sql
@@ -1,2 +1,4 @@
 
 INSERT INTO `ai_model_factory` VALUES (1, 'szj', 1, 'http://192.168.1.89:20000/synthesize');
+INSERT INTO `ai_model_factory` VALUES (2, 'mh', 1, NULL);
+INSERT INTO `ai_model_factory` VALUES (3, 'sy', 1, NULL);

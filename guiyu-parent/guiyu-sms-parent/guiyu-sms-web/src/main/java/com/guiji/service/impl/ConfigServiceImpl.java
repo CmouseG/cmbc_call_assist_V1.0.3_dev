b57aec67e6d7bbb76d584a8498ddaec2df86045f
孙东波 <867677564@qq.com>
2019-03-21 15:33:44 +0800

    公共方法改造

--- guiyu-parent/guiyu-sms-parent/guiyu-sms-web/src/main/java/com/guiji/service/impl/ConfigServiceImpl.java
+++ guiyu-parent/guiyu-sms-parent/guiyu-sms-web/src/main/java/com/guiji/service/impl/ConfigServiceImpl.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guiji.auth.api.IAuth;
-import com.guiji.component.result.Result;
 import com.guiji.component.result.Result.ReturnData;
 import com.guiji.service.ConfigService;
 import com.guiji.sms.dao.SmsConfigMapper;
@@ -22,14 +19,12 @@ import com.guiji.sms.vo.ConfigListRspVO;
 import com.guiji.sms.vo.ConfigReqVO;
 import com.guiji.sms.vo.SmsConfigVO;
 import com.guiji.user.dao.entity.SysOrganization;
-import com.guiji.user.dao.entity.SysUser;
+import com.guiji.utils.NameUtil;
 import com.guiji.utils.RedisUtil;
 
 @Service
 public class ConfigServiceImpl implements ConfigService
 {
-	private static final Logger logger = LoggerFactory.getLogger(ConfigServiceImpl.class);
-	
 	@Autowired
 	IAuth auth;
 	@Autowired
@@ -80,7 +75,7 @@ public class ConfigServiceImpl implements ConfigService
 		configVO.setAuditingStatus(config.getAuditingStatus());
 		configVO.setRunStatus(config.getRunStatus());
 		configVO.setCompanyName(config.getCompanyName());
-		configVO.setCreateUser(getUserName(config.getCreateId().toString()));
+		configVO.setCreateUser(NameUtil.getUserName(config.getCreateId().toString()));
 		configVO.setCreateTime(config.getCreateTime());
 		return configVO;
 	}
@@ -197,27 +192,5 @@ public class ConfigServiceImpl implements ConfigService
 								.andTemplateIdEqualTo(templateId);
 		Long count = configMapper.countByExample(example);
 		return count.intValue();
-	}
-	
-	
-	public String getUserName(String userId) {
-		String cacheName = (String) redisUtil.get(userId);
-		if (cacheName != null) {
-			return cacheName;
-		} else {
-			try {
-				Result.ReturnData<SysUser> result = auth.getUserById(Long.valueOf(userId));
-				if(result!=null && result.getBody()!=null) {
-					String userName = result.getBody().getUsername();
-					if (userName != null) {
-						redisUtil.set(userId, userName);
-						return userName;
-					}
-				}
-			} catch (Exception e) {
-				logger.error(" auth.getUserName error :" + e);
-			}
-		}
-		return "";
 	}
 }

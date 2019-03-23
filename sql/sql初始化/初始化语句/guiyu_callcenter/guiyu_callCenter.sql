634dac478ba0babcbf21db242fb6d2ba9b6f96c2
liyang <821927329@qq.com>
2019-03-23 20:59:03 +0800

    Upload new file

---
+++ sql/sql初始化/初始化语句/guiyu_callcenter/guiyu_callCenter.sql
@@ -0,0 +1,12 @@
+ 
+use guiyu_callcenter;
+
+
+ 
+ALTER TABLE `call_out_plan` add COLUMN originate_cmd VARCHAR(500);
+
+ALTER TABLE `call_out_plan_0` add COLUMN originate_cmd VARCHAR(500);
+
+ALTER TABLE `call_out_plan_1` add COLUMN originate_cmd VARCHAR(500);
+ 
+ 

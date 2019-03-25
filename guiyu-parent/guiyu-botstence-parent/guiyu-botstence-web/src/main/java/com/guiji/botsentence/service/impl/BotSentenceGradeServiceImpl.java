cb4cf7b79d6ba2323b2b5461e9eadf467e73bd6a
zhangpeng <zhangpeng@guiji.ai>
2019-03-25 15:40:27 +0800

    回退权限改造内容

--- guiyu-parent/guiyu-botstence-parent/guiyu-botstence-web/src/main/java/com/guiji/botsentence/service/impl/BotSentenceGradeServiceImpl.java
+++ guiyu-parent/guiyu-botstence-parent/guiyu-botstence-web/src/main/java/com/guiji/botsentence/service/impl/BotSentenceGradeServiceImpl.java
@@ -148,10 +148,18 @@ public class BotSentenceGradeServiceImpl implements IBotSentenceGradeService {
 					evaluate = "接通时长" + detail.getValue2() + detail.getValue3();
 				}
 			}else if("05".equals(detail.getType())) {//触发业务问答次数
-				if(StringUtils.isNotBlank(evaluate)) {
-					evaluate = evaluate + " and ('一般问题' in 经过域 and 一般问题['进入次数']" + detail.getValue2() + detail.getValue3() + ")"; 
+				if("0".equals(detail.getValue3())) {//如果触 发业务问答次数等于0 
+					if(StringUtils.isNotBlank(evaluate)) {
+						evaluate = evaluate + " and (一般问题['进入次数']" + detail.getValue2() + detail.getValue3() + ")"; 
+					}else {
+						evaluate = " 一般问题['进入次数']" + detail.getValue2() + detail.getValue3(); 
+					}
 				}else {
-					evaluate = "'一般问题' in 经过域 and 一般问题['进入次数']" + detail.getValue2() + detail.getValue3(); 
+					if(StringUtils.isNotBlank(evaluate)) {
+						evaluate = evaluate + " and ('一般问题' in 经过域 and 一般问题['进入次数']" + detail.getValue2() + detail.getValue3() + ")"; 
+					}else {
+						evaluate = "'一般问题' in 经过域 and 一般问题['进入次数']" + detail.getValue2() + detail.getValue3(); 
+					}
 				}
 			}else if("06".equals(detail.getType())) {//触发业务问答次数
 				if(StringUtils.isNotBlank(evaluate)) {

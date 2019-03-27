d5f48669870c656aedc59acb199218af9e65f998
liyang <821927329@qq.com>
2019-03-27 11:29:04 +0800

    点击协呼，需要等待机器人说完再转过去

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/controller/AssistCallController.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/controller/AssistCallController.java
@@ -7,6 +7,7 @@ import com.guiji.calloutserver.api.IAssistCall;
 import com.guiji.calloutserver.constant.Constant;
 import com.guiji.calloutserver.enm.ECallState;
 import com.guiji.calloutserver.entity.AIRequest;
+import com.guiji.calloutserver.entity.Channel;
 import com.guiji.calloutserver.eventbus.event.AsrCustomerEvent;
 import com.guiji.calloutserver.eventbus.event.ToAgentEvent;
 import com.guiji.calloutserver.fs.LocalFsServer;
@@ -15,6 +16,7 @@ import com.guiji.calloutserver.helper.RobotNextHelper;
 import com.guiji.calloutserver.manager.AIManager;
 import com.guiji.calloutserver.manager.ToAgentManager;
 import com.guiji.calloutserver.service.CallOutPlanService;
+import com.guiji.calloutserver.service.ChannelService;
 import com.guiji.component.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +47,8 @@ public class AssistCallController implements IAssistCall {
     AsyncEventBus asyncEventBus;
     @Autowired
     ChannelHelper channelHelper;
+    @Autowired
+    ChannelService channelService;
     //注册这个监听器
     @PostConstruct
     public void register() {
@@ -107,6 +111,23 @@ public class AssistCallController implements IAssistCall {
             return Result.error("0305015");
         }
 
+        log.info("开始判定是否需要等待，转协呼,callId[{}]",callId);
+        Channel channel = channelService.findByUuid(callId);
+        if (channel != null && channel.isInPlay() && channel.getExpectEndPlayTime() != null) {
+            long millNow = new Date().getTime();
+            if (channel.getExpectEndPlayTime() > millNow) {
+                channel.setIsMediaLock(true); //通道锁住，不允许再播放其他录音
+                long watiMill = channel.getExpectEndPlayTime() - millNow;
+                log.info("将等待[{}]毫秒后再转协呼,callId[{}]",watiMill,callId);
+                try {
+                    Thread.sleep(watiMill);
+                } catch (InterruptedException e) {
+                    log.error("线程sleep出现异常",e);
+                }
+            }
+        }
+
+        log.info("时间已到，开始转协呼,callId[{}]",callId);
         CallOutPlan callPlan = new CallOutPlan();
         callPlan.setCallId(bigInteCallId);
         callPlan.setAgentStartTime(new Date());

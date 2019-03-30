53de6b172977ca6ab30bd11461dcb51c4e298823
qianxin <qixiworld@163.com>
2019-03-30 15:46:24 +0800

    JAVAR-325 机器人实时查询增加用户查询、分页查询

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/impl/RobotServiceImpl.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/impl/RobotServiceImpl.java
@@ -3,6 +3,7 @@ package com.guiji.dispatch.impl;
 import com.guiji.auth.api.IAuth;
 import com.guiji.botsentence.api.IBotSentenceProcess;
 import com.guiji.botsentence.api.entity.BotSentenceProcess;
+import com.guiji.dispatch.bean.UserLineBotenceVO;
 import com.guiji.dispatch.constant.RedisConstant;
 import com.guiji.dispatch.dao.ext.RobotMapper;
 import com.guiji.dispatch.dto.DispatchRobotOpDto;
@@ -18,6 +19,7 @@ import com.guiji.dispatch.util.ResHandler;
 import com.guiji.dispatch.vo.DispatchRobotOpVo;
 import com.guiji.robot.model.UserResourceCache;
 import com.guiji.user.dao.entity.SysUser;
+import com.guiji.utils.JsonUtils;
 import com.guiji.utils.RedisUtil;
 import com.netflix.discovery.converters.Auto;
 import org.apache.commons.lang3.StringUtils;
@@ -27,10 +29,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 机器人Service
@@ -53,61 +52,115 @@ public class RobotServiceImpl implements RobotService {
     private RedisUtil redisUtil;
 
     @Override
-    public List<DispatchRobotOpVo> queryDispatchRobotOp() {
-        List<DispatchRobotOpVo> list = new ArrayList<DispatchRobotOpVo>();
+    public List<DispatchRobotOpVo> queryDispatchRobotOp(ResultPage<DispatchRobotOpVo> page) {
+        List<DispatchRobotOpVo> pageList = new LinkedList<DispatchRobotOpVo>();
         Map<Object,Object> map = redisUtil.hmget(RedisConstant.RedisConstantKey.ROBOT_USER_RESOURCE);
         if(null != map){
-            for(Map.Entry<Object, Object> entry: map.entrySet()){
-                DispatchRobotOpVo robot = new DispatchRobotOpVo();
+            //有序treemap
+            Map<Object, Object> treeMap = new TreeMap<>(map);
+            int len = 0;
+            List<DispatchRobotOpVo> list = new ArrayList<DispatchRobotOpVo>();
+            for(Map.Entry<Object, Object> entry: treeMap.entrySet()){
                 String userId = (String)entry.getKey();
+                SysUser user = ResHandler.getResObj(iAuth.getUserById(Long.valueOf(userId)));
                 UserResourceCache res = (UserResourceCache)entry.getValue();
                 if(null != res){
                     Integer maxRobotNum = res.getAiNum();
-                    if(null != maxRobotNum && maxRobotNum > 0) {
+                    if(null != maxRobotNum && maxRobotNum>0) {
                         Map<String, Integer> tempAiNumMap = res.getTempAiNumMap();
                         if (null != tempAiNumMap) {
                             for (Map.Entry<String, Integer> aiNum : tempAiNumMap.entrySet()) {
                                 String botstenceId = aiNum.getKey();
                                 if(!StringUtils.isEmpty(botstenceId)) {
-                                    SysUser user = ResHandler.getResObj(iAuth.getUserById(Long.valueOf(userId)));
+                                    DispatchRobotOpVo robot = new DispatchRobotOpVo();
+                                    robot.setUserId(userId);
                                     robot.setUserName(null != user?user.getUsername():null);
-                                    List<BotSentenceProcess> botList = ResHandler.getBotsentenceResObj(iBotSentenceProcess.getTemplateById(botstenceId));
-                                    if(null != botList){
-                                        robot.setBotstenceName(botList.get(0).getTemplateName());
-                                    }
-                                    Integer robotNum = aiNum.getValue();
+                                    robot.setBotstenceName(this.getBotstenceName(botstenceId));
+                                //    Integer robotNum = aiNum.getValue();
                                     robot.setBotstenceId(botstenceId);
-                                    robot.setRobotNum(robotNum);
+                                    robot.setMaxRobotNum(aiNum.getValue());
+                                    robot.setRobotNum(this.getActualRobotCount(Integer.valueOf(userId), botstenceId));
+                                    list.add(robot);
+                                    len++;
                                 }else{
                                     continue;
                                 }
                             }
                         }
-                    }else{
-                        continue;
                     }
-                    robot.setMaxRobotNum(maxRobotNum);
                 }
-                robot.setUserId(userId);
-                list.add(robot);
+            }
+
+            int pageNo = page.getPageNo();
+            int pageSize = page.getPageSize();
+            int startIdx = (pageNo - 1) * pageSize + 1;
+            int endIdx = pageNo * pageSize;
+            endIdx = endIdx<len?endIdx:(startIdx<len?len:-1);
+            startIdx = startIdx<len?startIdx:-1;
+            if(startIdx>0) {
+                for (int idx = startIdx; idx <= endIdx; idx++) {
+                    pageList.add(list.get(idx-1));
+                }
             }
         }
 
         //匹配人工补充机器人数
         List<DispatchRobotOp> opList = robotMapper.queryDisRobotOpList(null, null);
-        if(null != list && list.size()>0
+        if(null != pageList && pageList.size()>0
             && null != opList && opList.size()>0){
-            for(DispatchRobotOpVo robot: list){
-                for(DispatchRobotOp opRobot: opList){
+            loopA : for(DispatchRobotOpVo robot: pageList){
+                loopB : for(DispatchRobotOp opRobot: opList){
                     if(robot.getUserId().equals(opRobot.getUserId())
                         && robot.getBotstenceId().equals(opRobot.getBotstenceId())){
                         robot.setSupplNum(opRobot.getSupplNum());
+                        continue  loopA;
                     }
                 }
             }
         }
+        return pageList;
+    }
 
-        return list;
+    /**
+     * 获取模板名称
+     * @param botstenceId
+     * @return
+     */
+    private String getBotstenceName(String botstenceId){
+        String botstenceName = null;
+        Object obj = redisUtil.get(RedisConstant.RedisConstantKey.TMP_BOTSTENCENAME + botstenceId);
+        if(null != obj){
+            botstenceName = (String)obj;
+        }else {
+            List<BotSentenceProcess> botList
+                    = ResHandler.getBotsentenceResObj(iBotSentenceProcess.getTemplateById(botstenceId));
+            if (null != botList && botList.size() > 0) {
+                botstenceName = null != botList.get(0) ? botList.get(0).getTemplateName() : null;
+                redisUtil.set(RedisConstant.RedisConstantKey.TMP_BOTSTENCENAME + botstenceId, botstenceName,
+                        RedisConstant.RedisConstantKey.TMP_BOTSTENCENAME_TIMEOUT);
+            }
+        }
+        return botstenceName;
+    }
+
+    /**
+     * 实际动态分配数量
+     * @param userId
+     * @param botstenceId
+     * @return
+     */
+    private Integer getActualRobotCount(Integer userId, String botstenceId){
+        Integer actualRobotCount = 0;
+        List<UserLineBotenceVO> userLineRobotList = (List<UserLineBotenceVO>)redisUtil.get(RedisConstant.RedisConstantKey.REDIS_USER_ROBOT_LINE_MAX_PLAN);
+        if(null != userLineRobotList){
+            for(UserLineBotenceVO ulb: userLineRobotList){
+                if(userId == ulb.getUserId() && botstenceId.equals(ulb.getBotenceName())){
+                    actualRobotCount = ulb.getMaxRobotCount();
+                    break;
+                }
+            }
+        }
+        return actualRobotCount;
     }
 
     @Override

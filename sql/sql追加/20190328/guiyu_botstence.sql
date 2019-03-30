1887b8f90bce172feb0b19b744d966f25772b116
zhangpeng <zhangpeng@guiji.ai>
2019-03-30 17:54:47 +0800

    添加字段脚本

--- sql/sql追加/20190328/guiyu_botstence.sql
+++ sql/sql追加/20190328/guiyu_botstence.sql
@@ -1,1 +1,4 @@
 ALTER TABLE guiyu_botstence.bot_sentence_grade_rule MODIFY COLUMN remark VARCHAR(256) COMMENT '意向备注';
+
+ALTER TABLE guiyu_botstence.bot_publish_sentence_log ADD (org_code VARCHAR(32));
+ALTER TABLE guiyu_botstence.bot_publish_sentence_log ADD (org_name VARCHAR(64));

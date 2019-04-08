4d9fb43e35cba89c3e5615524f8060871a2e3011
qinghua <qinghua0310@hotmail.com>
2019-04-08 10:31:11 +0800

    sql更新

--- sql/sql追加/hotfix_0404/linemarket.sql
+++ sql/sql追加/hotfix_0404/linemarket.sql
@@ -4,3 +4,10 @@ alter table sip_line_base_info
 
 alter table sip_line_exclusive
 	add fee_or_not tinyint(1) null comment '0免费1收费' after end_date;
+
+
+update sip_line_exclusive set fee_or_not = true where line_fee_type=1;
+update sip_line_base_info set fee_or_not = true where line_fee_type=1;
+
+update sip_line_exclusive set fee_or_not = false where line_fee_type=2;
+update sip_line_base_info set fee_or_not = false where line_fee_type=2;

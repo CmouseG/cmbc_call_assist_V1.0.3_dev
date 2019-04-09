4db5f48ede1c082199501ba3d074f7b7c207a4d2
tangtaiquan <tangtaiquan@guiji.ai>
2019-04-09 15:30:33 +0800

    Update 01-guiyu_base.sql

--- sql/sql追加/20190328/权限改造说明/01-guiyu_base.sql
+++ sql/sql追加/20190328/权限改造说明/01-guiyu_base.sql
@@ -85,4 +85,4 @@ INSERT INTO sys_product (id, name, product_desc, product_status, style, sys_name
 INSERT INTO sys_product (id, name, product_desc, product_status, style, sys_name, logo, crt_user, crt_time, update_time, update_user) VALUES (3, '招行马拉松', '招行-马拉松', 1, null, null, null, 1, '2019-03-27 00:00:00', '2019-03-27 00:00:00', 1);
 INSERT INTO sys_product (id, name, product_desc, product_status, style, sys_name, logo, crt_user, crt_time, update_time, update_user) VALUES (4, '翼看', '翼看', 1, null, null, null, 1, '2019-03-27 00:00:00', '2019-03-27 00:00:00', 1);
 
-UPDATE sys_organization t SET t.`open` = 1 WHERE t.type = 1
+UPDATE sys_organization t SET t.`open` = 1 WHERE t.type = 1;

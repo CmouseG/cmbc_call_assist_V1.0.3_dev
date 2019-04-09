6b624749d67addec2ef277ed98c3bd359f1f235a
孙东波 <867677564@qq.com>
2019-04-09 17:07:57 +0800

    auth 开户报错缺陷修改

--- guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/service/OrganizationService.java
+++ guiyu-parent/guiyu-auth-parent/guiyu-auth-web/src/main/java/com/guiji/auth/service/OrganizationService.java
@@ -736,9 +736,13 @@ public class OrganizationService {
 	
 	/**
 	 * 获取上一级组织
+	 * 系统的上一级就是自己
 	 */
 	public SysOrganization getParentOrg(String orgCode)
 	{
+		if("1".equals(orgCode)){
+			 return this.getOrgByCode(orgCode);
+		}
 		String parentOrgCode = orgCode.substring(0, orgCode.lastIndexOf(".")).substring(0, orgCode.substring(0, orgCode.lastIndexOf(".")).lastIndexOf(".")+1);
 		parentOrgCode = "1.".equals(parentOrgCode)? "1" : parentOrgCode;
 		return this.getOrgByCode(parentOrgCode);

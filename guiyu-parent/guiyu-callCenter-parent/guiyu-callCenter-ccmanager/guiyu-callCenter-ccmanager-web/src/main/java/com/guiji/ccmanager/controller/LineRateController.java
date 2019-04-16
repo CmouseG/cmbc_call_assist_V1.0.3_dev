b3dfb6c961389bdefef8b403e7126350151d9b1f
qianxin <qixiworld@163.com>
2019-04-16 18:54:39 +0800

    JAVAR-1 getRateAll接口入参

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/controller/LineRateController.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/controller/LineRateController.java
@@ -2,10 +2,12 @@ package com.guiji.ccmanager.controller;
 
 import com.guiji.ccmanager.api.ILineRate;
 import com.guiji.ccmanager.entity.LineRateResponse;
+import com.guiji.ccmanager.entity.RateTimeReq;
 import com.guiji.ccmanager.service.LineRateService;
 import com.guiji.component.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,10 +36,9 @@ public class LineRateController implements ILineRate {
     }
 
     @Override
-    public Result.ReturnData<List<LineRateResponse>> getLineRateAll(@RequestParam(value = "startTime") Date startTime,
-                                                                    @RequestParam(value = "endTime") Date endTime){
+    public Result.ReturnData<List<LineRateResponse>> getLineRateAll(@RequestBody RateTimeReq rateTime){
 
-        List<LineRateResponse> list =lineRateService.getLineRateAll(startTime, endTime);
+        List<LineRateResponse> list =lineRateService.getLineRateAll(rateTime.getStartTime(), rateTime.getEndTime());
 
         return Result.ok(list);
     }

83636edd64fcc94b0baf879af99b4b94e6e6b78e
liyang <821927329@qq.com>
2019-05-11 09:23:30 +0800

    分表

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/scheduler/CallCenterDayReportJobHandler.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/scheduler/CallCenterDayReportJobHandler.java
@@ -5,11 +5,13 @@ import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
 import com.xxl.job.core.log.XxlJobLogger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @JobHandler(value="CallCenterDayReportJobHandler")
 @Component
+@Slf4j
 public class CallCenterDayReportJobHandler extends IJobHandler {
 
 	@Autowired
@@ -18,7 +20,9 @@ public class CallCenterDayReportJobHandler extends IJobHandler {
 	@Override
 	public ReturnT<String> execute(String param) {
 		XxlJobLogger.log("呼叫中心，按天统计report_call_day，CallCenterDayReportJobHandler开始");
+		log.info("按天统计开始");
 		reportSchedulerService.reportCallDayScheduler();
+		log.info("按天统计结束");
 		XxlJobLogger.log("呼叫中心，按天统计report_call_day，CallCenterDayReportJobHandler结束");
 		return SUCCESS;
 	}

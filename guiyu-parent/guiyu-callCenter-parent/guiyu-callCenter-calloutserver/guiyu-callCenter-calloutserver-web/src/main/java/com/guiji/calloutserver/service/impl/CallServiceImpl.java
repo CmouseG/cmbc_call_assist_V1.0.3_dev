db485223fae04f51e200b3605297bd44295072c1
liyang <821927329@qq.com>
2019-05-14 09:25:20 +0800

    呼叫增加codec参数

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/service/impl/CallServiceImpl.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/service/impl/CallServiceImpl.java
@@ -12,6 +12,7 @@ import com.guiji.dict.api.ISysDict;
 import com.guiji.dict.vo.SysDictVO;
 import com.guiji.fsmanager.entity.FsLineInfoVO;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -52,6 +53,7 @@ public class CallServiceImpl implements CallService {
     public void makeCall(CallOutPlan callplan, String recordFile) {
         //获取线路服务器
         FsLineInfoVO fsLine = fsLineManager.getFsLine(callplan.getLineId());
+        String codec = fsLine.getCodec();
 
         String ip = fsLine.getFsIp();
         if(ip.contains(":")){
@@ -60,7 +62,9 @@ public class CallServiceImpl implements CallService {
         String callid = callplan.getCallId().toString();
         Integer orgId = callplan.getOrgId();
         //构建外呼命令
-        String cmd = String.format("originate {origination_uuid=%s,origination_caller_id_name=%s}" +
+        String cmd = String.format("originate {" +
+                        (StringUtils.isNotEmpty(codec) ? "absolute_codec_string="+codec+"," : "") +
+                        "origination_uuid=%s,origination_caller_id_name=%s}" +
                         "sofia/internal/%s@%s:%s 'start_asr:%s %s" +
                         ", record_session:/usr/local/freeswitch/recordings/%s, park' inline",
                 callid,

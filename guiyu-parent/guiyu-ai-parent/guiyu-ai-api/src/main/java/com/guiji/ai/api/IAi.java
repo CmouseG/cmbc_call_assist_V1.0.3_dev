89405c56924b741abc4ce84b46d631365faf1ddb
孙东波 <867677564@qq.com>
2019-05-16 14:45:11 +0800

    guiyu-ai

--- guiyu-parent/guiyu-ai-parent/guiyu-ai-api/src/main/java/com/guiji/ai/api/IAi.java
+++ guiyu-parent/guiyu-ai-parent/guiyu-ai-api/src/main/java/com/guiji/ai/api/IAi.java
@@ -1,7 +1,6 @@
 package com.guiji.ai.api;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -33,6 +32,6 @@ public interface IAi {
 	/**
 	 * 根据busiId查询TTS处理结果
 	 */
-	@GetMapping(value = "getResultByBusId")
+	@PostMapping(value = "getResultByBusId")
 	public ReturnData<TtsRsp> getResultByBusId(String busId);
 }
--- guiyu-parent/guiyu-ai-parent/guiyu-ai-web/src/main/java/com/guiji/ai/controller/AiController.java
+++ guiyu-parent/guiyu-ai-parent/guiyu-ai-web/src/main/java/com/guiji/ai/controller/AiController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,7 +72,7 @@ public class AiController implements IAi
 	/**
 	 * 查询异步合成结果
 	 */
-	@GetMapping(value = "getResultByBusId")
+	@PostMapping(value = "getResultByBusId")
 	public ReturnData<TtsRsp> getResultByBusId(String busId)
 	{
 		return Result.ok(ai.getResultByBusId(busId));

2e36d60601ac3641ce3859e86db03aec56d7745d
qianxin <qixiworld@163.com>
2019-05-21 14:35:16 +0800

    JAVAR-994 任务中心导入增加所属单位和姓名

--- guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/pushcallcenter/PushPhonesHandlerImpl.java
+++ guiyu-parent/guiyu-dispatch-parent/guiyu-dispatch-web/src/main/java/com/guiji/dispatch/pushcallcenter/PushPhonesHandlerImpl.java
@@ -125,9 +125,12 @@ public class PushPhonesHandlerImpl implements IPushPhonesHandler {
                                 GateWayLineOccupyVo occupyLine = null;
 								try {
 									BeanUtils.copyProperties(callBean, dispatchRedis);
-									callBean.setTempId(dispatchRedis.getRobot());
-									callBean.setAgentGroupId(dispatchRedis.getCallAgent());
-									callBean.setRemarks(dispatchRedis.getAttach());
+									callBean.setTempId(dispatchRedis.getRobot());			//模板
+									callBean.setAgentGroupId(dispatchRedis.getCallAgent());	//坐席组
+									callBean.setRemarks(dispatchRedis.getAttach());			//参数
+									callBean.setAnswerUser(dispatchRedis.getCustName());	//客户名称
+									callBean.setEnterprise(dispatchRedis.getCustCompany());	//客户所属单位
+									callBean.setImportTime(dispatchRedis.getGmtCreate());	//导入时间
 									List<Integer> lines = new ArrayList<>();
 
 

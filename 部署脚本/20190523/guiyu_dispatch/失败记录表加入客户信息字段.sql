f077931b0365f5bff72adef481b98366fa971fc1
qianxin <qixiworld@163.com>
2019-05-23 13:34:22 +0800

    Update 失败记录表加入客户信息字段.sql

--- 部署脚本/20190523/guiyu_dispatch/失败记录表加入客户信息字段.sql
+++ 部署脚本/20190523/guiyu_dispatch/失败记录表加入客户信息字段.sql
@@ -1,3 +1,3 @@
-ALTER TABLE file_error_records
+ALTER TABLE guiyu_dispatch.file_error_records
 	ADD cust_name            VARCHAR(32) COMMENT '客户姓名',
 	ADD cust_company         VARCHAR(64) COMMENT '客户所属单位';

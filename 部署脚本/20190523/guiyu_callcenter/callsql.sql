e361f1bb03ae4494df1f9eea6f7681e63c3fef96
liyang <821927329@qq.com>
2019-05-23 09:58:25 +0800

    Update callsql.sql

--- 部署脚本/20190523/guiyu_callcenter/callsql.sql
+++ 部署脚本/20190523/guiyu_callcenter/callsql.sql
@@ -1,4 +1,4 @@
 call addCallPlanColumn();
 
 ALTER TABLE call_out_plan
- ADD COLUMN enterprise VARCHAR(32) COMMENT '用户所属企业单位',ADD COLUMN answer_user VARCHAR(32) COMMENT '用户名称',ADD COLUMN import_time DATETIME COMMENT '导入时间';
+ ADD COLUMN enterprise VARCHAR(64) COMMENT '用户所属企业单位',ADD COLUMN answer_user VARCHAR(32) COMMENT '用户名称',ADD COLUMN import_time DATETIME COMMENT '导入时间';

55ced9ca062e112596894e1b5de995aafe818af5
liyang <821927329@qq.com>
2019-06-06 18:14:05 +0800

    修改异步线程池配置

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/CalloutServerApplication.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-calloutserver/guiyu-callCenter-calloutserver-web/src/main/java/com/guiji/calloutserver/CalloutServerApplication.java
@@ -1,11 +1,14 @@
 package com.guiji.calloutserver;
 
+import com.guiji.component.config.ThreadAsyncConfig;
 import com.guiji.component.result.EnableAutoResultPack;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoResultPack
 @EnableSwagger2
 @SpringBootApplication(scanBasePackages = "com.guiji")
+@ComponentScan(basePackages = "com.guiji", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ThreadAsyncConfig.class))
 @EnableScheduling
 @EnableFeignClients(basePackages = "com.guiji")
 //@MapperScan("com.guiji.*.dao")

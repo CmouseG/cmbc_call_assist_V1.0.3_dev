8b6e9cb78ea67c96ea1624341cc26771410ca74a
lixiong <lixiong@guiji.ai>
2019-06-10 09:57:57 +0800

    deploy config

--- guiyu-parent/guiyu-botstence-parent/guiyu-botstence-web/src/main/resources/botstence-SQL修改.sql
+++
@@ -1,6 +0,0 @@
-use guiyu_botstence;
-
-alter table volice_info
-	add tts_composite_type int default 1 null comment 'tts合成类型：1-变量;2-变量加内容' after need_tts;
-
-alter table bot_sentence_process alter column sound_type set default 'mh';

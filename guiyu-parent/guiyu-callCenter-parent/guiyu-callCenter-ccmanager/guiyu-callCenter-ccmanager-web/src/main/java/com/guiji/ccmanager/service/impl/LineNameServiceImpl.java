2626cc98aee541bf3bbbb5df665e3ab99609ebfe
liyang <821927329@qq.com>
2019-06-10 16:23:56 +0800

    线路名称从新街口获取

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/LineNameServiceImpl.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/LineNameServiceImpl.java
@@ -19,25 +19,25 @@ import org.springframework.stereotype.Service;
 public class LineNameServiceImpl implements LineNameService {
 
 
-    @Autowired
-    RedisUtil redisUtil;
+//    @Autowired
+//    RedisUtil redisUtil;
     @Autowired
     LineMarketRemote lineMarketRemote;
 
     @Override
     public String getLineName(Integer lineId, Integer customerId) {
 
-        Object o = redisUtil.get("callCenter_ccmanger_lineName_"+lineId+"_"+customerId);
-        if(o!=null){
-            return (String) o;
-        }else{
+//        Object o = redisUtil.get("callCenter_ccmanger_lineName_"+lineId+"_"+customerId);
+//        if(o!=null){
+//            return (String) o;
+//        }else{
             //请求线路市场获取线路名称
             try{
-                Result.ReturnData<SipLineVO> result = lineMarketRemote.queryUserSipLineByLineId(String.valueOf(customerId),lineId);
-                log.info("queryUserSipLineByLineId返回:",result.getCode());
+                Result.ReturnData<SipLineVO> result = lineMarketRemote.queryLineNameByLineId(String.valueOf(customerId),lineId);
+//                log.info("queryUserSipLineByLineId返回:",result.getCode());
                 if(result.success){
                     String lineName = result.getBody().getLineName();
-                    redisUtil.set("callCenter_ccmanger_lineName_"+lineId+"_"+customerId,lineName,86400);
+//                    redisUtil.set("callCenter_ccmanger_lineName_"+lineId+"_"+customerId,lineName,86400);
                     return lineName;
                 }
             }catch (Exception e){
@@ -45,7 +45,7 @@ public class LineNameServiceImpl implements LineNameService {
                 return "";
             }
 
-        }
+//        }
         return "";
     }
 

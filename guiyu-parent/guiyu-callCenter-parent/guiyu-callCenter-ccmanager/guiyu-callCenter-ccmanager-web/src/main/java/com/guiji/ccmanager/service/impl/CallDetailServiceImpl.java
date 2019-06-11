e3dbd6985fe89f20f9ca1b38c527e14e77e880a3
liyang <821927329@qq.com>
2019-06-11 09:43:11 +0800

    解决分页的问题

--- guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/CallDetailServiceImpl.java
+++ guiyu-parent/guiyu-callCenter-parent/guiyu-callCenter-ccmanager/guiyu-callCenter-ccmanager-web/src/main/java/com/guiji/ccmanager/service/impl/CallDetailServiceImpl.java
@@ -262,19 +262,11 @@ public class CallDetailServiceImpl implements CallDetailService {
         if(authLevel == AuthLevelEnum.USER.getLevel()){
             myCallOutPlanQueryEntity.setOrgId(orgId);
             myCallOutPlanQueryEntity.setCustomerId(Integer.valueOf(customerId));
-            /*long longCustomerId = Long.valueOf(customerId);
-            if(authService.isSeat(longCustomerId)) {//具有人工坐席权限
-                AgentExample agentExample = new AgentExample();
-                agentExample.createCriteria().andCustomerIdEqualTo(longCustomerId);
-                List<Agent> listAgent = agentMapper.selectByExample(agentExample);
-                if(listAgent!=null && listAgent.size()>0){
-                    myCallOutPlanQueryEntity.setAgentId(String.valueOf(listAgent.get(0).getUserId()));
-                }
-            }*/
-            String agentId = authService.getAgentIdByCustomerId(customerId);
+
+           /* String agentId = authService.getAgentIdByCustomerId(customerId);
             if(StringUtils.isNotEmpty(agentId)){
                 myCallOutPlanQueryEntity.setAgentId(agentId);
-            }
+            }*/
         }else if(authLevel == AuthLevelEnum.ORG.getLevel()){
             myCallOutPlanQueryEntity.setOrgId(orgId);
         }else if(authLevel == AuthLevelEnum.ORG_EXT.getLevel()){

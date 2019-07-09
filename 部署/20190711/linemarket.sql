4999a6ce2f042eeb75a8b9bc0c28559f05799abc
peiqinghua <peiqinghua@guiji.ai>
2019-07-09 16:14:57 +0800

    Update linemarket.sql

--- 部署/20190711/linemarket.sql
+++ 部署/20190711/linemarket.sql
@@ -1,2 +1,2 @@
 alter table sip_line_base_info
-	add non_local_prefix varchar(20) null comment '外显归属地前缀' after non_local_prefix;
+	add non_local_prefix varchar(20) null comment '外显归属地前缀' after belong_org_code;
